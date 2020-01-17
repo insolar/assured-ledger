9fbeb9f6161f2038e95732b13a5d2cdf09820f56
Red Cyrax <cyraxred@users.noreply.github.com>
2020-01-17 18:05:04 +0300

    Converted the endless test into main()

--- ledger-core/v2/conveyor/main/conveyor_test_main.go
+++ ledger-core/v2/conveyor/main/conveyor_test_main.go
@@ -14,18 +14,166 @@
 //    limitations under the License.
 //
 
-package conveyor
+package main
 
 import (
 	"context"
 	"fmt"
+	"math"
 	"reflect"
 	"runtime"
 	"strings"
+	"time"
 
+	conveyor2 "github.com/insolar/assured-ledger/ledger-core/v2/conveyor"
 	"github.com/insolar/assured-ledger/ledger-core/v2/conveyor/smachine"
 	"github.com/insolar/assured-ledger/ledger-core/v2/log/logfmt"
+	"github.com/insolar/assured-ledger/ledger-core/v2/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/injector"
+	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/longbits"
 )
+
+func noError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	machineConfig := smachine.SlotMachineConfig{
+		SlotPageSize:      1000,
+		PollingPeriod:     500 * time.Millisecond,
+		PollingTruncate:   1 * time.Millisecond,
+		ScanCountLimit:    100000,
+		SlotAliasRegistry: &conveyor2.GlobalAliases{},
+	}
+
+	factoryFn := func(pn pulse.Number, v conveyor2.InputEvent) smachine.CreateFunc {
+		return func(ctx smachine.ConstructionContext) smachine.StateMachine {
+			sm := &AppEventSM{eventValue: v, pn: pn}
+			return sm
+		}
+	}
+	machineConfig.SlotMachineLogger = conveyorSlotMachineLogger{}
+
+	conveyor := conveyor2.NewPulseConveyor(context.Background(), conveyor2.PulseConveyorConfig{
+		ConveyorMachineConfig: machineConfig,
+		SlotMachineConfig:     machineConfig,
+		EventlessSleep:        1 * time.Second,
+		MinCachePulseAge:      100,
+		MaxPastPulseAge:       1000,
+	}, factoryFn, nil)
+
+	pd := pulse.NewFirstPulsarData(10, longbits.Bits256{})
+
+	conveyor.StartWorker(nil, nil)
+
+	noError(conveyor.CommitPulseChange(pd.AsRange()))
+	eventCount := 0
+
+	time.Sleep(10 * time.Millisecond)
+
+	for i := 0; i < 100; i++ {
+		pd = pd.CreateNextPulsarPulse(10, func() longbits.Bits256 {
+			return longbits.Bits256{}
+		})
+		fmt.Println(">>>================================== ", pd, " ====================================")
+		noError(conveyor.PreparePulseChange(nil))
+		time.Sleep(100 * time.Millisecond)
+		noError(conveyor.CommitPulseChange(pd.AsRange()))
+		fmt.Println("<<<================================== ", pd, " ====================================")
+		time.Sleep(10 * time.Millisecond)
+
+		eventToCall := ""
+		if eventCount < math.MaxInt32 {
+			eventCount++
+			noError(conveyor.AddInput(context.Background(), pd.NextPulseNumber(), fmt.Sprintf("event-%d-future", eventCount)))
+			eventCount++
+			noError(conveyor.AddInput(context.Background(), pd.PrevPulseNumber(), fmt.Sprintf("event-%d-past", eventCount)))
+
+			for j := 0; j < 1; j++ {
+				eventCount++
+				eventToCall = fmt.Sprintf("event-%d-present", eventCount)
+				noError(conveyor.AddInput(context.Background(), pd.PulseNumber, fmt.Sprintf("event-%d-present", eventCount)))
+			}
+		}
+
+		time.Sleep(time.Second)
+
+		if eventToCall != "" {
+			link := conveyor.GetPublishedGlobalAlias(eventToCall)
+			//require.False(t, link.IsEmpty())
+			smachine.ScheduleCallTo(link, func(callContext smachine.MachineCallContext) {
+				fmt.Println("Global call: ", callContext.GetMachineId(), link)
+			}, false)
+		}
+
+	}
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("======================")
+	conveyor.StopNoWait()
+	time.Sleep(time.Hour)
+}
+
+/* ===================================================== */
+
+type AppEventSM struct {
+	smachine.StateMachineDeclTemplate
+
+	pulseSlot *conveyor2.PulseSlot
+
+	pn         pulse.Number
+	eventValue interface{}
+	expiry     time.Time
+}
+
+func (sm *AppEventSM) GetStateMachineDeclaration() smachine.StateMachineDeclaration {
+	return sm
+}
+
+func (sm *AppEventSM) InjectDependencies(_ smachine.StateMachine, _ smachine.SlotLink, injector *injector.DependencyInjector) {
+	injector.MustInject(&sm.pulseSlot)
+}
+
+func (sm *AppEventSM) GetInitStateFor(machine smachine.StateMachine) smachine.InitFunc {
+	if sm != machine {
+		panic("illegal value")
+	}
+	fmt.Println("new: ", sm.eventValue, sm.pn)
+	return sm.stepInit
+}
+
+func (sm *AppEventSM) stepInit(ctx smachine.InitializationContext) smachine.StateUpdate {
+	ctx.Log().Trace(fmt.Sprint("init: ", sm.eventValue, sm.pn))
+	ctx.SetDefaultMigration(sm.migrateToClosing)
+	ctx.PublishGlobalAlias(sm.eventValue)
+
+	return ctx.Jump(sm.stepRun)
+}
+
+func (sm *AppEventSM) stepRun(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	ctx.Log().Trace(fmt.Sprint("run: ", sm.eventValue, sm.pn, sm.pulseSlot.PulseData()))
+	ctx.LogAsync().Trace(fmt.Sprint("(via async log) run: ", sm.eventValue, sm.pn, sm.pulseSlot.PulseData()))
+	return ctx.Poll().ThenRepeat()
+}
+
+func (sm *AppEventSM) migrateToClosing(ctx smachine.MigrationContext) smachine.StateUpdate {
+	sm.expiry = time.Now().Add(2600 * time.Millisecond)
+	ctx.Log().Trace(fmt.Sprint("migrate: ", sm.eventValue, sm.pn))
+	ctx.SetDefaultMigration(nil)
+	return ctx.Jump(sm.stepClosingRun)
+}
+
+func (sm *AppEventSM) stepClosingRun(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	if su, wait := ctx.WaitAnyUntil(sm.expiry).ThenRepeatOrElse(); wait {
+		ctx.Log().Trace(fmt.Sprint("wait: ", sm.eventValue, sm.pn))
+		return su
+	}
+	ctx.Log().Trace(fmt.Sprint("stop: ", sm.eventValue, sm.pn, "late=", time.Since(sm.expiry)))
+	return ctx.Stop()
+}
+
+/* ===================================================== */
 
 type conveyorSlotMachineLogger struct {
 }
