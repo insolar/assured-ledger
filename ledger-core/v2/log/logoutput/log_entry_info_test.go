dc6d68148e8f56133b1f69a59fe40958595ae900
Cyrax Red <github.com@cyrax.red>
2020-02-13 00:24:24 +0300

    Performance improvement for caller func

---
+++ ledger-core/v2/log/logoutput/log_entry_info_test.go
@@ -0,0 +1,35 @@
+package logoutput
+
+import (
+	"github.com/stretchr/testify/require"
+	"runtime"
+	"testing"
+)
+
+func TestGetCallerInfo(t *testing.T) {
+	fl1, fn1, ln1 := getCallerInfo1(0)
+	fl2, fn2, ln2 := getCallerInfo2(0)
+	require.Equal(t, fl1, fl2)
+	require.Equal(t, fn1, fn2)
+	require.Equal(t, ln1+1, ln2)
+}
+
+func BenchmarkGetCallerInfo(b *testing.B) {
+	b.Run("Caller+FuncForPC", func(b *testing.B) {
+		for i := b.N; i > 0; i-- {
+			fl, fn, ln := getCallerInfo1(0)
+			if fl == "" && fn == "" && ln == 0 {
+				runtime.KeepAlive(fl)
+			}
+		}
+	})
+
+	b.Run("Callers+FuncForPC", func(b *testing.B) {
+		for i := b.N; i > 0; i-- {
+			fl, fn, ln := getCallerInfo2(0)
+			if fl == "" && fn == "" && ln == 0 {
+				runtime.KeepAlive(fl)
+			}
+		}
+	})
+}
