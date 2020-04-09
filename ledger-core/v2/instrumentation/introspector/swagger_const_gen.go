fdad6296dafbb8f926036555a0d790023bac2fa1
Eugene Blikh <bigbes@gmail.com>
2020-04-09 13:35:13 +0300

    PLAT-200: update licenses

--- ledger-core/v2/instrumentation/introspector/swagger_const_gen.go
+++ ledger-core/v2/instrumentation/introspector/swagger_const_gen.go
@@ -1,3 +1,8 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
 package introspector
 
 /*
@@ -6,7 +11,7 @@ This code was generated automatically using _swagger-helper
 */
 
 const (
-publisherSwagger = `{
+	publisherSwagger = `{
   "swagger": "2.0",
   "info": {
     "title": "instrumentation/introspector/introproto/publisher.proto",
