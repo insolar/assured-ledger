1fda433de30cb4194c820c136b1e9ee57bce0770
Red Cyrax <cyraxred@users.noreply.github.com>
2020-04-09 18:39:33 +0300

    Added Semaphore

---
+++ ledger-core/v2/vanilla/synckit/semaphore.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package synckit
+
+import (
+	"time"
+
+	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/throw"
+)
+
+func NewSemaphore(limit int) Semaphore {
+	if limit <= 0 {
+		panic(throw.IllegalValue())
+	}
+	return Semaphore{make(chan struct{}, limit)}
+}
+
+type Semaphore struct {
+	sema chan struct{}
+}
+
+func (v Semaphore) Lock() {
+	v.sema <- struct{}{}
+}
+
+func (v Semaphore) TryLock() bool {
+	select {
+	case v.sema <- struct{}{}:
+		return true
+	default:
+		return false
+	}
+}
+
+func (v Semaphore) LockTimeout(d time.Duration) bool {
+	return v.LockExt(d, nil)
+}
+
+func (v Semaphore) LockExt(d time.Duration, done <-chan struct{}) bool {
+	select {
+	case v.sema <- struct{}{}:
+		return true
+	case <-done:
+		return false
+	default:
+		select {
+		case v.sema <- struct{}{}:
+			return true
+		case <-done:
+			return false
+		case <-time.After(d):
+		}
+		return false
+	}
+}
+
+func (v Semaphore) Unlock() {
+	_ = <-v.sema
+}
+
+func (v Semaphore) Close() {
+	close(v.sema)
+}
