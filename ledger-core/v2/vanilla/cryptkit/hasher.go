144e775bb6a7b529cf2894bf162bef25cd2f5bdf
Red Cyrax <cyraxred@users.noreply.github.com>
2020-04-09 18:38:34 +0300

    Added HashingReader

--- ledger-core/v2/vanilla/cryptkit/hasher.go
+++ ledger-core/v2/vanilla/cryptkit/hasher.go
@@ -9,6 +9,7 @@ import (
 	"hash"
 	"io"
 
+	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/iokit"
 	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/longbits"
 	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/throw"
 )
@@ -51,5 +52,55 @@ func DigestOfHash(digester BasicDigester, hasher hash.Hash) Digest {
 		panic(throw.IllegalValue())
 	} else {
 		return NewDigest(longbits.NewMutableFixedSize(h), digester.GetDigestMethod())
+	}
+}
+
+func NewHashingTeeReader(hasher DigestHasher, r io.Reader) HashingTeeReader {
+	tr := HashingTeeReader{}
+	tr.BasicDigester = hasher.BasicDigester
+	tr.CopyTo = hasher.Hash
+	tr.R = r
+	return tr
+}
+
+type HashingTeeReader struct {
+	BasicDigester
+	iokit.TeeReader
+}
+
+func (v HashingTeeReader) SumToDigest() Digest {
+	return DigestOfHash(v.BasicDigester, v.CopyTo.(hash.Hash))
+}
+
+func (v *HashingTeeReader) SumToDigestAndSignature(m SigningMethod) (d Digest, s Signature, err error) {
+	d = DigestOfHash(v.BasicDigester, v.CopyTo.(hash.Hash))
+	s, err = v.ReadSignature(m)
+	return
+}
+
+func (v *HashingTeeReader) ReadSignatureBytes() ([]byte, error) {
+	b := make([]byte, v.GetDigestSize())
+	n, err := io.ReadFull(&v.TeeReader, b)
+	return b[:n], err
+}
+
+func (v *HashingTeeReader) ReadSignature(m SigningMethod) (Signature, error) {
+	if b, err := v.ReadSignatureBytes(); err != nil {
+		return Signature{}, err
+	} else {
+		return NewSignature(longbits.NewMutableFixedSize(b), v.GetDigestMethod().SignedBy(m)), nil
+	}
+}
+
+func (v *HashingTeeReader) ReadAndVerifySignature(verifier DataSignatureVerifier) ([]byte, error) {
+	d := DigestOfHash(v.BasicDigester, v.CopyTo.(hash.Hash))
+	if b, err := v.ReadSignatureBytes(); err != nil {
+		return nil, err
+	} else {
+		s := NewSignature(longbits.NewMutableFixedSize(b), verifier.GetSignatureMethod())
+		if !verifier.IsValidDigestSignature(d, s) {
+			err = throw.RemoteBreach("packet signature mismatch")
+		}
+		return b, err
 	}
 }
