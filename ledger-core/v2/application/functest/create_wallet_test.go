a080bceec87db5bf3b71c7d5fc4512c2e62b4b83
Ekaterina Rogushkova <catherine.zamaraeva@gmail.com>
2020-04-13 17:15:13 +0300

    PLAT-203: add count dependecy on network size

--- ledger-core/v2/application/functest/create_wallet_test.go
+++ ledger-core/v2/application/functest/create_wallet_test.go
@@ -9,28 +9,16 @@ package functest
 
 import (
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func getCount(args []string) (int, error) {
-	for _, s := range args {
-		if strings.Contains(s, "test.count") {
-			countInfo := strings.Split(s, "=")
-			return strconv.Atoi(countInfo[1])
-		}
-	}
-	return 1, nil
-}
-
 // Creates wallets and check Reference format in response body.
 func TestCreateWallet(t *testing.T) {
-	count, err := getCount(os.Args)
-	require.NoError(t, err, "failed to parse -test.count param")
+	status := getStatus(t)
+	require.NotEqual(t, 0, status.WorkingListSize, "not enough nodes to run test")
+	count := 50 * status.WorkingListSize // 250 for 5 nodes
 
 	t.Run(fmt.Sprintf("count=%d", count), func(t *testing.T) {
 		for i := 0; i < count; i++ {
@@ -40,7 +28,7 @@ func TestCreateWallet(t *testing.T) {
 
 			resp, err := unmarshalCreateWalletResponse(rawResp)
 			require.NoError(t, err, "failed to unmarshal response")
-			require.Nil(t, resp.Err, "problem during execute request")
+			require.Empty(t, resp.Err, "problem during execute request")
 			require.Contains(t, resp.Ref, "insolar:", "wrong reference")
 			require.NotEmpty(t, resp.TraceID, "traceID mustn't be empty")
 		}
