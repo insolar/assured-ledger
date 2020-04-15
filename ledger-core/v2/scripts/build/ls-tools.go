a0c86ab4e80c22865559cd9775ca7b3aeca98233
Aleksander Alekseev <mail@eax.me>
2020-04-15 14:59:02 +0300

    NOISSUE: fix ls-tools.go

--- ledger-core/v2/scripts/build/ls-tools.go
+++ ledger-core/v2/scripts/build/ls-tools.go
@@ -1,10 +1,10 @@
+//usr/bin/env go run -mod=vendor "$0" "$@"; exit "$?"
+// ls-tools.go - go script extract tools imports from tools.go
+
 // Copyright 2020 Insolar Network Ltd.
 // All rights reserved.
 // This material is licensed under the Insolar License version 1.0,
 // available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
-
-//usr/bin/env go run -mod=vendor "$0" "$@"; exit "$?"
-// ls-tools.go - go script extract tools imports from tools.go
 
 // +build tools
 
