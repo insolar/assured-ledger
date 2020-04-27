400e4d5aec353cae8bfe95140440a9dfdb47f2bb
Mikhail V. Zhukov <mikhail.v.zhukov@gmail.com>
2020-04-27 11:29:46 +0300

    PLAT-278: remove code which was generated for unit test

--- ledger-core/v2/conveyor/smachine/api_context.go
+++ ledger-core/v2/conveyor/smachine/api_context.go
@@ -255,8 +255,6 @@ type PostInitStepContext interface {
 	InOrderStepContext
 }
 
-//go:generate minimock -i github.com/insolar/assured-ledger/ledger-core/v2/conveyor/smachine.ExecutionContext -o ./ -s _mock.go -g
-
 type ExecutionContext interface {
 	PostInitStepContext
 
