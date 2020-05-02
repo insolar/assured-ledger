52892fc9734e94ddf12c0a90d4dc9b717e9633eb
Red Cyrax <cyraxred@users.noreply.github.com>
2020-05-02 21:04:42 +0300

    Further work on serialization

--- ledger-core/v2/rms/proto_gen.go
+++ ledger-core/v2/rms/proto_gen.go
@@ -3,8 +3,6 @@
 // This material is licensed under the Insolar License version 1.0,
 // available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
 
-// +build windows
+package rms
 
-package insproto
-
-//go:generate protoc -I=. -I=$GOPATH/src --gogofaster_out=Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:./ ins.proto
+// go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
