5bf2f8b1721a2f4ab7ca3a95c1f2c74ead9393c6
Sergey Morozov <sergey.morozov@insolar.io>
2020-05-13 21:04:15 +0300

    NOISSUE: generated serialization proto (#150)
    * generated serialization proto
    * updated `setup-go` action to add GOBIN to PATH
    * added protoc-gen-gogofaster to tools.go as it is used for generating proto options

--- ledger-core/v2/rms/proto_gen.go
+++ ledger-core/v2/rms/proto_gen.go
@@ -5,4 +5,6 @@
 
 package rms
 
+// `go:generate` command below is here just for information. It should not be used by go generate.
+// Actual generation is in the Makefile.
 // go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
