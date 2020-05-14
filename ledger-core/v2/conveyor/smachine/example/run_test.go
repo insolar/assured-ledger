c9ac37e0ca5335f9fcba28ba79a672f2d6332c34
Red Cyrax <cyraxred@users.noreply.github.com>
2020-05-15 01:12:45 +0300

    Move example run and test

--- ledger-core/v2/conveyor/smachine/example/run_test.go
+++ ledger-core/v2/conveyor/smachine/example/run_test.go
@@ -3,10 +3,10 @@
 // This material is licensed under the Insolar License version 1.0,
 // available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
 
-package main
+package example
 
 import "testing"
 
-func TestPlayerScenario(t *testing.T) {
-	main()
+func TestExample(t *testing.T) {
+	RunExample()
 }
