a4314ab91a34748e6f7323b18bb007c0ce3c0789
Andrey Bronin <andrey.bronin@insolar.io>
2020-05-19 13:31:00 +0300

    TestSMExecute_UpdateSawRequests

--- ledger-core/v2/virtual/execute/execute_test.go
+++ ledger-core/v2/virtual/execute/execute_test.go
@@ -91,3 +91,111 @@ func TestSMExecute_IncreasePendingCounter(t *testing.T) {
 	assert.Equal(t, uint8(1), smObject.PotentialMutablePendingCount)
 	assert.Equal(t, uint8(0), smObject.PotentialImmutablePendingCount)
 }
+
+func TestSMExecute_UpdateSawRequests(t *testing.T) {
+	var (
+		ctx = inslogger.TestContext(t)
+		mc  = minimock.NewController(t)
+
+		pd              = pulse.NewFirstPulsarData(10, longbits.Bits256{})
+		pulseSlot       = conveyor.NewPresentPulseSlot(nil, pd.AsRange())
+		catalog         = object.NewCatalogMock(mc)
+		smObjectID      = gen.IDWithPulse(pd.PulseNumber)
+		smGlobalRef     = reference.NewSelf(smObjectID)
+		smObject        = object.NewStateMachineObject(smGlobalRef)
+		sharedStateData = smachine.NewUnboundSharedData(&smObject.SharedState)
+
+		callFlags payload.CallRequestFlags
+	)
+
+	defer mc.Finish()
+
+	callFlags.SetTolerance(payload.CallTolerable)
+	callFlags.SetState(payload.CallDirty)
+
+	smExecute := SMExecute{
+		Payload: &payload.VCallRequest{
+			Polymorph:           uint32(payload.TypeVCallRequest),
+			CallType:            payload.CTConstructor,
+			CallFlags:           callFlags,
+			CallSiteDeclaration: testwallet.GetPrototype(),
+			CallSiteMethod:      "New",
+			CallOutgoing:        smObjectID,
+			Arguments:           insolar.MustSerialize([]interface{}{}),
+		},
+		objectCatalog: catalog,
+		pulseSlot:     &pulseSlot,
+	}
+
+	stepChecker := testutils.NewSMStepChecker()
+	{
+		exec := SMExecute{}
+		stepChecker.AddStep(exec.stepCheckRequest)
+		stepChecker.AddStep(exec.stepUpdateSawRequests)
+		stepChecker.AddStep(exec.stepUpdatePendingCounters)
+		stepChecker.AddStep(exec.stepWaitObjectReady)
+	}
+
+	execCtx := smachine.NewExecutionContextMock(mc).
+		GetContextMock.Return(ctx).
+		JumpMock.Set(testutils.CheckWrapper(stepChecker, t)).
+		UseSharedMock.Set(CallSharedDataAccessor)
+
+
+
+	// execCtx.SetDefaultMigrationMock.Return()
+
+	smObjectAccessor := object.SharedStateAccessor{SharedDataLink: sharedStateData}
+	catalog.GetOrCreateMock.Expect(execCtx, smGlobalRef).Return(smObjectAccessor)
+
+	smExecute.Init(execCtx)
+	smExecute.stepGetObject(execCtx)
+
+
+	assert.Empty(t,  smObject.SawRequests)
+
+	smExecute.stepUpdateSawRequests(execCtx)
+	// smExecute.stepUpdatePendingCounters(execCtx)
+
+	// _, ok := smObject.SawRequests[smGlobalRef]
+	// fmt.Println("smGlobalRef ", smGlobalRef.String())
+	// assert.True (t, ok, "reference not found in SawRequests")
+	assert.Len(t, smObject.SawRequests, 1)
+
+	// getBalance SM ==================================
+
+	smExecuteGetBalance := SMExecute{
+		Payload: &payload.VCallRequest{
+			Polymorph:           uint32(payload.TypeVCallRequest),
+			CallType:            payload.CTMethod,
+			CallFlags:           callFlags,
+			CallSiteDeclaration: testwallet.GetPrototype(),
+			CallSiteMethod:      "GetBalance",
+			CallOutgoing:        smObjectID,
+			Arguments:           insolar.MustSerialize([]interface{}{}),
+		},
+		objectCatalog: catalog,
+		pulseSlot:     &pulseSlot,
+	}
+
+	stepChecker2 := testutils.NewSMStepChecker()
+	{
+		exec := SMExecute{}
+		stepChecker2.AddStep(exec.stepCheckRequest)
+		stepChecker2.AddStep(exec.stepUpdateSawRequests)
+		stepChecker2.AddStep(exec.stepUpdatePendingCounters)
+		stepChecker2.AddStep(exec.stepWaitObjectReady)
+	}
+
+	execCtx2 := smachine.NewExecutionContextMock(mc).
+		GetContextMock.Return(ctx).
+		JumpMock.Set(testutils.CheckWrapper(stepChecker2, t)).
+		UseSharedMock.Set(CallSharedDataAccessor)
+
+	smExecuteGetBalance.Init(execCtx2)
+	smExecuteGetBalance.stepGetObject(execCtx2)
+	smExecuteGetBalance.stepUpdateSawRequests(execCtx2)
+	// smExecuteGetBalance.stepUpdatePendingCounters(execCtx2)
+
+	assert.Len(t, smObject.SawRequests, 2)
+}
