54191700a777920f27663b4707abd863f86f9aa3
anton.alekseev <anton.a.alekseev@gmail.com>
2020-05-22 17:10:17 +0300

    PLAT-295 penging table struct

---
+++ ledger-core/v2/virtual/object/pending_table.go
@@ -0,0 +1,75 @@
+package object
+
+import (
+	"github.com/insolar/assured-ledger/ledger-core/v2/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/v2/reference"
+)
+
+type pendingTable struct {
+	Ordered   pendingList
+	Unordered pendingList
+}
+
+type pendingList struct {
+	oldestPulse pulse.Number
+	requests    map[reference.Global]*struct{
+		active bool
+	}
+}
+
+func newPendingTable() pendingTable {
+	return pendingTable{
+		Ordered:   newPendingList(),
+		Unordered: newPendingList(),
+	}
+}
+
+func newPendingList() pendingList {
+	return pendingList{
+		requests: make(map[reference.Global]*struct{active bool}),
+	}
+}
+
+// Add adds reference.Global and update OldestPulse if needed
+// returns true if added and false if already exists
+func (pt *pendingList) Add(ref reference.Global) bool {
+	if _, exist := pt.requests[ref]; exist {
+		return false
+	}
+
+	pt.requests[ref] = &struct{active bool}{true}
+
+	requestPulseNumber := ref.GetLocal().GetPulseNumber()
+	if pt.oldestPulse == 0 || requestPulseNumber < pt.oldestPulse {
+		pt.oldestPulse = requestPulseNumber
+	}
+
+	return true
+}
+
+func (pt *pendingList) Finish(ref reference.Global) bool {
+	if _, exist := pt.requests[ref]; !exist {
+		return false
+	}
+	pt.requests[ref].active = false
+
+	return true
+}
+
+func (pt *pendingList) Count() int {
+	return len(pt.requests)
+}
+
+func (pt *pendingList) CountFinish() int {
+	var count int
+	for _, request := range pt.requests {
+		if !request.active {
+			count++
+		}
+	}
+	return count
+}
+
+func (pt *pendingList) OldestPulse() pulse.Number {
+	return pt.oldestPulse
+}
---
+++ ledger-core/v2/virtual/object/pending_table_test.go
@@ -0,0 +1,52 @@
+package object
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/insolar/assured-ledger/ledger-core/v2/insolar/gen"
+	"github.com/insolar/assured-ledger/ledger-core/v2/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/v2/reference"
+	"github.com/insolar/assured-ledger/ledger-core/v2/vanilla/longbits"
+)
+
+func TestPendingList(t *testing.T) {
+	pd := pulse.NewFirstPulsarData(10, longbits.Bits256{})
+	currentPulse := pd.PulseNumber
+
+	objectOld := gen.IDWithPulse(currentPulse)
+	RefOld := reference.NewSelf(objectOld)
+
+	nextPulseNumber := currentPulse+pulse.Number(pd.NextPulseDelta)
+	objectOne := gen.IDWithPulse(nextPulseNumber)
+	objectTwo := gen.IDWithPulse(nextPulseNumber)
+	RefOne := reference.NewSelf(objectOne)
+	RefTwo := reference.NewSelf(objectTwo)
+
+
+	pt := newPendingList()
+	require.Equal(t, 0, pt.Count())
+	require.Equal(t, 0, pt.CountFinish())
+	require.Equal(t, pulse.Number(0), pt.oldestPulse)
+
+	require.Equal(t, true, pt.Add(RefOne))
+	require.Equal(t, 1, pt.Count())
+	require.Equal(t, nextPulseNumber, pt.oldestPulse)
+
+	require.Equal(t, false, pt.Add(RefOne))
+	require.Equal(t, 1, pt.Count())
+	require.Equal(t, nextPulseNumber, pt.oldestPulse)
+
+	require.Equal(t, true, pt.Add(RefOld))
+	require.Equal(t, 2, pt.Count())
+	require.Equal(t, pd.PulseNumber, pt.oldestPulse)
+
+	require.Equal(t, true, pt.Add(RefTwo))
+	require.Equal(t, 3, pt.Count())
+	require.Equal(t, pd.PulseNumber, pt.oldestPulse)
+	require.Equal(t, 0, pt.CountFinish())
+
+	pt.Finish(RefOld)
+	require.Equal(t, 1, pt.CountFinish())
+}
