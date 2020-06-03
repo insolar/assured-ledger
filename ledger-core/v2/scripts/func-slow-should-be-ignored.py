4fd51ec9beda72aa98cf63eb6359c77b80c3b968
Aleksander Alekseev <mail@eax.me>
2020-06-03 11:51:39 +0300

    fix

--- ledger-core/v2/scripts/func-slow-should-be-ignored.py
+++ ledger-core/v2/scripts/func-slow-should-be-ignored.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import time
+import datetime
 import sys
 
 # `make test_func_slow` errors should be ignored until ~28.06.2020
 # this is an estimated date when Release 0 Milestone 3 will be ready
-if time.time() > 1593332989:
+m3date = "28.06.2020"
+tup = datetime.datetime.strptime(m3date, "%d.%m.%Y").timetuple()
+if time.time() > time.mktime(tup):
 	sys.exit(1) # false, don't ignore
 
 # true, tests should be ignored
