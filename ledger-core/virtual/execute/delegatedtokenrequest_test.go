668bda70a2244e262099fc9ae168778c7c6dc30d
Mikhail V. Zhukov <mikhail.v.zhukov@gmail.com>
2020-06-09 19:09:06 +0300

    PLAT-309: Test refactoring

--- ledger-core/virtual/execute/delegatedtokenrequest_test.go
+++ ledger-core/virtual/execute/delegatedtokenrequest_test.go
@@ -18,8 +18,7 @@ import (
 	"github.com/insolar/assured-ledger/ledger-core/insolar/node"
 	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
 	"github.com/insolar/assured-ledger/ledger-core/instrumentation/inslogger"
-	"github.com/insolar/assured-ledger/ledger-core/network/messagesender"
-	"github.com/insolar/assured-ledger/ledger-core/network/messagesender/adapter"
+	messageSender "github.com/insolar/assured-ledger/ledger-core/network/messagesender"
 	"github.com/insolar/assured-ledger/ledger-core/pulse"
 	"github.com/insolar/assured-ledger/ledger-core/reference"
 	"github.com/insolar/assured-ledger/ledger-core/testutils/gen"
@@ -36,70 +35,41 @@ func TestVirtual_CDelegatedCallRequest(t *testing.T) {
 
 	slotMachine := slotdebugger.New(ctx, t, true)
 	slotMachine.PrepareRunner(ctx, mc)
+	slotMachine.PrepareMockedMessageSender(mc)
 
 	var (
-		caller      = gen.UniqueReference()
-		callee      = gen.UniqueReference()
-		outgoing    = gen.UniqueIDWithPulse(slotMachine.PulseSlot.CurrentPulseNumber())
-		objectRef   = reference.NewSelf(outgoing)
-		sharedState = &object.SharedState{
-			Info: object.Info{
-				Reference:      objectRef,
-				PendingTable:   object.NewRequestTable(),
-				KnownRequests:  object.NewRequestTable(),
-				ReadyToWork:    smsync.NewConditional(1, "ReadyToWork").SyncLink(),
-				OrderedExecute: smsync.NewConditional(1, "MutableExecution").SyncLink(),
+		caller    = gen.UniqueReference()
+		callee    = gen.UniqueReference()
+		outgoing  = gen.UniqueIDWithPulse(slotMachine.PulseSlot.CurrentPulseNumber())
+		objectRef = reference.NewSelf(outgoing)
+
+		smExecute = SMExecute{
+			Payload: &payload.VCallRequest{
+				CallType:     payload.CTConstructor,
+				CallFlags:    payload.BuildCallFlags(contract.CallTolerable, contract.CallDirty),
+				CallOutgoing: outgoing,
+
+				Caller: caller,
+				Callee: callee,
 			},
 		}
 	)
-	smExecute := SMExecute{
-		Payload: &payload.VCallRequest{
-			CallType:     payload.CTConstructor,
-			CallFlags:    payload.BuildCallFlags(contract.CallTolerable, contract.CallDirty),
-			CallOutgoing: outgoing,
-
-			Caller: caller,
-			Callee: callee,
-		},
-		Meta: &payload.Meta{
-			Sender: caller,
-		},
-	}
-
-	var messageSender adapter.MessageSender = adapter.NewMessageSenderMock(mc).
-		PrepareAsyncMock.Set(
-		func(ctx smachine.ExecutionContext, fn adapter.AsyncCallFunc) smachine.AsyncCallRequester {
-			var bargeInRef reference.Global
-			srv := messagesender.NewServiceMock(mc).
-				SendRoleMock.Set(
-				func(ctx context.Context, msg payload.Marshaler, role node.DynamicRole, object reference.Global, pn pulse.Number, opts ...messagesender.SendOption) (err error) {
-					req, ok := msg.(*payload.VDelegatedCallRequest)
-					require.True(t, ok)
-					bargeInRef = req.RequestReference
-					return nil
-				})
-			fn(ctx.GetContext(), srv)
-			require.False(t, bargeInRef.IsZero())
-			slotLink, bargeInHolder := ctx.GetPublishedGlobalAliasAndBargeIn(DelegationTokenAwaitKey{bargeInRef})
-			require.False(t, slotLink.IsZero())
-			require.True(t, bargeInHolder.CallWithParam(&payload.VDelegatedCallResponse{
-				RefIn:              bargeInRef,
-				DelegatorSignature: deadBeef,
-			}))
-
-			return smachine.NewAsyncCallRequesterMock(mc).
-				WithoutAutoWakeUpMock.Set(
-				func() smachine.AsyncCallRequester {
-					return smachine.NewAsyncCallRequesterMock(t).StartMock.Set(func() {
-					})
-				})
-		})
-	slotMachine.AddInterfaceDependency(&messageSender)
 
 	catalogWrapper := object.NewCatalogMockWrapper(mc)
 
 	{
-		var catalog object.Catalog = catalogWrapper.Mock()
+		var (
+			catalog     object.Catalog = catalogWrapper.Mock()
+			sharedState                = &object.SharedState{
+				Info: object.Info{
+					Reference:      objectRef,
+					PendingTable:   object.NewRequestTable(),
+					KnownRequests:  object.NewRequestTable(),
+					ReadyToWork:    smsync.NewConditional(1, "ReadyToWork").SyncLink(),
+					OrderedExecute: smsync.NewConditional(1, "MutableExecution").SyncLink(),
+				},
+			}
+		)
 		slotMachine.AddInterfaceDependency(&catalog)
 
 		sharedStateData := smachine.NewUnboundSharedData(sharedState)
@@ -109,20 +79,40 @@ func TestVirtual_CDelegatedCallRequest(t *testing.T) {
 		catalogWrapper.AllowAccessMode(object.CatalogMockAccessGetOrCreate)
 	}
 
+	slotMachine.MessageSender.SendRole.Set(
+		func(_ context.Context, msg payload.Marshaler, role node.DynamicRole, object reference.Global, pn pulse.Number, _ ...messageSender.SendOption) error {
+			res, ok := msg.(*payload.VDelegatedCallRequest)
+			require.True(t, ok)
+			require.NotNil(t, res)
+			require.Equal(t, objectRef, object)
+			require.Equal(t, slotMachine.PulseSlot.CurrentPulseNumber(), pn)
+			return nil
+		})
+
 	slotMachine.Start()
 	defer slotMachine.Stop()
-
 	smWrapper := slotMachine.AddStateMachine(ctx, &smExecute)
+	{
 
-	slotMachine.RunTil(smWrapper.BeforeStep(smExecute.stepExecuteStart))
-	slotMachine.Migrate()
-	slotMachine.RunTil(smWrapper.AfterStep(smExecute.stepGetDelegationToken))
-	var smVDelegatedCallRequest SMDelegatedTokenRequest
-	slotMachine.RunTil(smWrapper.AfterStep(smVDelegatedCallRequest.stepSendRequest))
-	slotMachine.RunTil(smWrapper.BeforeStep(smExecute.stepAfterTokenGet.Transition))
-	require.NotNil(t, smExecute.delegationTokenSpec)
-	require.Equal(t, deadBeef, smExecute.delegationTokenSign)
+		slotMachine.RunTil(smWrapper.BeforeStep(smExecute.stepExecuteStart))
+		slotMachine.Migrate()
+		slotMachine.RunTil(smWrapper.AfterStep(smExecute.stepGetDelegationToken))
+		var smVDelegatedCallRequest SMDelegatedTokenRequest
+		slotMachine.RunTil(smWrapper.AfterStep(smVDelegatedCallRequest.stepSendRequest))
+		slotLink, bargeInHolder := slotMachine.SlotMachine.GetPublishedGlobalAliasAndBargeIn(DelegationTokenAwaitKey{smExecute.execution.Outgoing})
 
+		require.False(t, slotLink.IsZero())
+		require.True(t, bargeInHolder.CallWithParam(&payload.VDelegatedCallResponse{
+			DelegationSpec:     payload.CallDelegationToken{Outgoing: smExecute.execution.Outgoing},
+			DelegatorSignature: deadBeef,
+		}))
+	}
+	{
+		slotMachine.RunTil(smWrapper.BeforeStep(smExecute.stepAfterTokenGet.Transition))
+
+		require.NotNil(t, smExecute.delegationTokenSpec)
+		require.Equal(t, deadBeef, smExecute.delegationTokenSign)
+	}
 	require.NoError(t, catalogWrapper.CheckDone())
 	mc.Finish()
 
