155c0ebd7382d8ffe7863d84c1d89ef719ae19fc
Mikhail V. Zhukov <mikhail.v.zhukov@gmail.com>
2020-06-15 12:49:30 +0300

    PLAT-466: fix test

--- ledger-core/virtual/statemachine/conveyor_dispatcher_test.go
+++ ledger-core/virtual/statemachine/conveyor_dispatcher_test.go
@@ -7,7 +7,6 @@ package statemachine
 
 import (
 	"context"
-	"errors"
 	"testing"
 	"time"
 
@@ -19,16 +18,8 @@ import (
 	"github.com/insolar/assured-ledger/ledger-core/insolar/dispatcher"
 	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
 	"github.com/insolar/assured-ledger/ledger-core/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
-
-func handleFactory(_ pulse.Number, _ pulse.Range, input conveyor.InputEvent) (pulse.Number, smachine.CreateFunc) {
-	switch input.(*DispatcherMessage).MessageMeta.Get("error_type") {
-	case "error":
-		return 0, nil
-	default:
-		panic(errors.New("panic"))
-	}
-}
 
 var (
 	machineConfig = smachine.SlotMachineConfig{
@@ -39,7 +30,7 @@ var (
 	}
 )
 
-func newDispatcherWithConveyor() dispatcher.Dispatcher {
+func newDispatcherWithConveyor(factoryFn conveyor.PulseEventFactoryFunc) dispatcher.Dispatcher {
 	ctx := context.Background()
 	pulseConveyor := conveyor.NewPulseConveyor(ctx, conveyor.PulseConveyorConfig{
 		ConveyorMachineConfig: machineConfig,
@@ -47,19 +38,19 @@ func newDispatcherWithConveyor() dispatcher.Dispatcher {
 		EventlessSleep:        0,
 		MinCachePulseAge:      100,
 		MaxPastPulseAge:       1000,
-	}, handleFactory, nil)
+	}, factoryFn, nil)
 	return NewConveyorDispatcher(pulseConveyor)
 }
 
 func TestConveyorDispatcher_ErrorUnmarshalHandling(t *testing.T) {
-	dispatcher := newDispatcherWithConveyor()
+	dispatcher := newDispatcherWithConveyor(nil)
 	require.NotPanics(t, func() {
 		require.NoError(t, dispatcher.Process(&message.Message{}))
 	})
 }
 
 func TestConveyorDispatcher_WrongMetaTypeHandling(t *testing.T) {
-	dispatcher := newDispatcherWithConveyor()
+	dispatcher := newDispatcherWithConveyor(nil)
 	req := payload.VCallRequest{}
 	pl, _ := req.Marshal()
 	require.NotPanics(t, func() {
@@ -68,7 +59,10 @@ func TestConveyorDispatcher_WrongMetaTypeHandling(t *testing.T) {
 }
 
 func TestConveyorDispatcher_PanicInAddInputHandling(t *testing.T) {
-	dispatcher := newDispatcherWithConveyor()
+	dispatcher := newDispatcherWithConveyor(
+		func(_ pulse.Number, _ pulse.Range, _ conveyor.InputEvent) (pulse.Number, smachine.CreateFunc, error) {
+			panic(throw.E("handler panic"))
+		})
 	meta := payload.Meta{Pulse: pulse.Number(pulse.MinTimePulse + 1)}
 	metaPl, _ := meta.Marshal()
 	require.NotPanics(t, func() {
@@ -77,7 +71,11 @@ func TestConveyorDispatcher_PanicInAddInputHandling(t *testing.T) {
 }
 
 func TestConveyorDispatcher_ErrorInAddInputHandling(t *testing.T) {
-	dispatcher := newDispatcherWithConveyor()
+	dispatcher := newDispatcherWithConveyor(
+		func(_ pulse.Number, _ pulse.Range, _ conveyor.InputEvent) (pulse.Number, smachine.CreateFunc, error) {
+			return 0, nil, throw.E("handler error")
+
+		})
 	meta := payload.Meta{Pulse: pulse.Number(pulse.MinTimePulse + 1)}
 	metaPl, _ := meta.Marshal()
 	metadata := message.Metadata{}
