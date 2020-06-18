04268b1816e82c0024b1827860e8c05a7d1f08f2
dgovorukhin <dmitriy.govorukhin@gmail.com>
2020-06-18 11:26:26 +0300

    PLAT-354: Fix after rebase

--- ledger-core/virtual/execute/delegatedtokenrequest_test.go
+++ ledger-core/virtual/execute/delegatedtokenrequest_test.go
@@ -63,8 +63,12 @@ func TestVDelegatedCallRequest(t *testing.T) {
 
 	{
 		catalogWrapper := object.NewCatalogMockWrapper(mc)
-		var catalog object.Catalog = catalogWrapper.Mock()
+		var (
+			catalog     object.Catalog         = catalogWrapper.Mock()
+			authService authentication.Service = authentication.NewServiceMock(t)
+		)
 		slotMachine.AddInterfaceDependency(&catalog)
+		slotMachine.AddInterfaceDependency(&authService)
 
 		sharedStateData := smachine.NewUnboundSharedData(&object.SharedState{
 			Info: object.Info{
