74175de4b29509915a3aa684b37efd29eadf7818
Andrey Zolin <crrrr@mail.ru>
2020-06-24 12:07:40 +0300

    PLAT-458: create contract test

--- ledger-core/load/create_contract_test_attack.go
+++ ledger-core/load/create_contract_test_attack.go
@@ -3,6 +3,8 @@ package load
 import (
 	"context"
 	"errors"
+	"github.com/insolar/assured-ledger/ledger-core/load/util"
+	"math/rand"
 	"time"
 
 	"github.com/skudasov/loadgen"
@@ -13,10 +15,32 @@ type CreateContractTestAttack struct {
 }
 
 func (a *CreateContractTestAttack) Setup(hc loadgen.RunnerConfig) error {
+	a.GetRunner().L.Infof("Setup attacker")
+	rand.Seed(time.Now().Unix())
+	util.HttpClient = util.CreateHTTPClient()
 	return nil
 }
 func (a *CreateContractTestAttack) Do(ctx context.Context) loadgen.DoResult {
 	time.Sleep(400 * time.Millisecond)
+	url := util.GetURL(util.WalletCreatePath, "", "")
+	rawResp, err := util.SendAPIRequest(url, nil)
+	if err != nil {
+		a.GetRunner().L.Error(err)
+		return loadgen.DoResult{
+			Error:        err,
+			RequestLabel: CreateContractTestLabel,
+		}
+	}
+	resp, err := util.UnmarshalWalletCreateResponse(rawResp)
+	if err != nil {
+		a.GetRunner().L.Error(err)
+		return loadgen.DoResult{
+			Error:        err,
+			RequestLabel: CreateContractTestLabel,
+		}
+	}
+	// store result
+	_ = a.PutData(resp)
 	return loadgen.DoResult{
 		Error:        errors.New(""),
 		RequestLabel: CreateContractTestLabel,
@@ -24,4 +48,12 @@ func (a *CreateContractTestAttack) Do(ctx context.Context) loadgen.DoResult {
 }
 func (a *CreateContractTestAttack) Clone(r *loadgen.Runner) loadgen.Attack {
 	return &CreateContractTestAttack{WithRunner: loadgen.WithRunner{R: r}}
+}
+
+func (a *CreateContractTestAttack) PutData(mo interface{}) error {
+	if mo != nil && a.R.Config.StoreData {
+		data := []string{mo.(*util.WalletCreateResponse).Ref}
+		loadgen.DefaultWriteCSV(a, data)
+	}
+	return nil
 }
