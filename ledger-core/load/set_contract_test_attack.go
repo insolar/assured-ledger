3765d69d87e7861f6c6d6fcfc56d5a863e325bd5
Andrey Zolin <crrrr@mail.ru>
2020-06-24 17:01:35 +0300

    PLAT-458: added get and set targets implementations

--- ledger-core/load/set_contract_test_attack.go
+++ ledger-core/load/set_contract_test_attack.go
@@ -2,8 +2,8 @@ package load
 
 import (
 	"context"
-	"time"
-
+	"errors"
+	"github.com/insolar/assured-ledger/ledger-core/load/util"
 	"github.com/skudasov/loadgen"
 )
 
@@ -12,10 +12,33 @@ type SetContractTestAttack struct {
 }
 
 func (a *SetContractTestAttack) Setup(hc loadgen.RunnerConfig) error {
+	util.HttpClient = util.CreateHTTPClient()
 	return nil
 }
 func (a *SetContractTestAttack) Do(ctx context.Context) loadgen.DoResult {
-	time.Sleep(200 * time.Millisecond)
+	if len(loadgen.DefaultReadCSV(a)) == 0 {
+		return loadgen.DoResult{
+			Error:        errors.New("not data for test"),
+			RequestLabel: GetContractTestLabel,
+		}
+	}
+	var addAmountURL string
+	if len(a.GetManager().GeneratorConfig.Generator.Target) == 0 {
+		// set default
+		addAmountURL = util.GetURL(util.WalletAddAmountPath, "", "")
+	} else {
+		addAmountURL = a.GetManager().GeneratorConfig.Generator.Target + util.WalletAddAmountPath
+	}
+
+	for _, reference := range loadgen.DefaultReadCSV(a) {
+		err := util.AddAmountToWallet(addAmountURL, reference, 100)
+		if err != nil {
+			return loadgen.DoResult{
+				Error:        err,
+				RequestLabel: GetContractTestLabel,
+			}
+		}
+	}
 	return loadgen.DoResult{
 		Error:        nil,
 		RequestLabel: SetContractTestLabel,
