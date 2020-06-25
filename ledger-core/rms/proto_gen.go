0e4b9d3318047db934d4e39eb51f4b19190e1da3
Red Cyrax <cyraxred@users.noreply.github.com>
2020-06-25 18:15:18 +0300

    Fix forgotten generate

--- ledger-core/rms/proto_gen.go
+++ ledger-core/rms/proto_gen.go
@@ -3,10 +3,8 @@
 // This material is licensed under the Insolar License version 1.0,
 // available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
 
-// +build windows
-
 package rms
 
 // `go:generate` command below is here just for information. It should not be used by go generate.
 // Actual generation is in the Makefile.
-//go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
+// go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
