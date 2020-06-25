ba2a882a859fd6cc2818f102c800f1a2efe72f35
Red Cyrax <cyraxred@users.noreply.github.com>
2020-06-25 17:04:41 +0300

    SMs

--- ledger-core/ledger/server/buildersvc/update_future.go
+++ ledger-core/ledger/server/buildersvc/update_future.go
@@ -7,7 +7,6 @@ package buildersvc
 
 import (
 	"github.com/insolar/assured-ledger/ledger-core/conveyor/smachine"
-	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
 
 type Future struct {
@@ -15,9 +14,9 @@ type Future struct {
 }
 
 func (p *Future) GetReadySync() smachine.SyncLink {
-	panic(throw.NotImplemented())
+
 }
 
 func (p *Future) IsCommitted() bool {
-	panic(throw.NotImplemented())
+
 }
