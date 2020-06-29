bad9dfd8710f37bbd77c647664c640bc7ce999dc
Red Cyrax <cyraxred@users.noreply.github.com>
2020-06-29 11:02:50 +0300

    Further work

--- ledger-core/ledger/server/lineage/record_typeset.go
+++ ledger-core/ledger/server/lineage/record_typeset.go
@@ -7,7 +7,24 @@ package lineage
 
 import (
 	"github.com/insolar/assured-ledger/ledger-core/vanilla/longbits"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
+
+func setOf(canFollow... RecordType) RecordTypeSet {
+	if len(canFollow) == 0 {
+		return RecordTypeSet{}
+	}
+
+	bb := longbits.NewBitBuilder(longbits.LSB, 32)
+	for _, mt := range canFollow {
+		if mt > maxRecordType {
+			panic(throw.IllegalValue())
+		}
+		bb.SetBit(int(mt), 1)
+	}
+	skip, b := bb.TrimZeros()
+	return RecordTypeSet{RecordType(skip<<3), b}
+}
 
 type RecordTypeSet struct {
 	ofs  RecordType
@@ -19,7 +36,7 @@ func (v RecordTypeSet) Has(typ RecordType) bool {
 		return false
 	}
 	typ -= v.ofs
-	if typ >= RecordType(len(v.mask)) {
+	if (typ >> 3) >= RecordType(len(v.mask)) {
 		return false
 	}
 	return v.mask.BitBool(int(typ))
@@ -28,3 +45,4 @@ func (v RecordTypeSet) Has(typ RecordType) bool {
 func (v RecordTypeSet) IsZero() bool {
 	return v.ofs == 0 && v.mask == nil
 }
+
