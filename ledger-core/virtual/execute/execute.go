516bd090bbf5ab5fc07cb71efda58c42d6b7a550
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2020-06-30 23:10:02 +0300

    PLAT-388: Implemented SMCallSummary  (#383)

--- ledger-core/virtual/execute/execute.go
+++ ledger-core/virtual/execute/execute.go
@@ -9,7 +9,6 @@ package execute
 
 import (
 	"context"
-
 	"github.com/insolar/assured-ledger/ledger-core/conveyor"
 	"github.com/insolar/assured-ledger/ledger-core/conveyor/smachine"
 	"github.com/insolar/assured-ledger/ledger-core/cryptography/platformpolicy"
