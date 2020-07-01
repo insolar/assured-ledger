e61b3f6dcec22ec178fd4e1c15c10c8586d39215
Red Cyrax <cyraxred@users.noreply.github.com>
2020-07-01 10:57:00 +0300

    Helper for minimock to report full stacktrace

---
+++ ledger-core/testutils/mocklog/wrap_t.go
@@ -0,0 +1,75 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package mocklog
+
+import (
+	"fmt"
+
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/args"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
+)
+
+type Tester interface {
+// 	minimock.Tester
+//	logcommon.TestingLogger
+
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Error(...interface{})
+	Errorf(format string, args ...interface{})
+	FailNow()
+
+	Helper()
+	Log(...interface{})
+}
+
+func T(t Tester) Tester {
+	if t == nil {
+		return nil
+	}
+	return stackReport{t}
+}
+
+type stackReport struct {
+	t Tester
+}
+
+func (v stackReport) FailNow() {
+	v.t.FailNow()
+}
+
+func (v stackReport) Helper() {
+	v.t.Helper()
+}
+
+func (v stackReport) Log(args ...interface{}) {
+	v.t.Helper()
+	v.t.Log(args...)
+}
+
+func (v stackReport) Error(a ...interface{}) {
+	v.t.Helper()
+	v.t.Error(args.AppendIntfArgs(a, v.stackText())...)
+}
+
+func (v stackReport) Errorf(format string, args ...interface{}) {
+	v.t.Helper()
+	v.t.Error(fmt.Sprintf(format, args...), v.stackText())
+}
+
+func (v stackReport) Fatal(a ...interface{}) {
+	v.t.Helper()
+	v.t.Fatal(args.AppendIntfArgs(a, v.stackText())...)
+}
+
+func (v stackReport) Fatalf(format string, args ...interface{}) {
+	v.t.Helper()
+	v.t.Fatal(fmt.Sprintf(format, args...), v.stackText())
+}
+
+func (v stackReport) stackText() string {
+	return throw.JoinStackText("", throw.CaptureStack(1))
+}
