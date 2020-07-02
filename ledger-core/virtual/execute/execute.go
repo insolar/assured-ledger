9ba7bad79c20c93e399613a762ae5778aad260d4
MariaVasilenko <28919295+MariaVasilenko@users.noreply.github.com>
2020-07-02 19:55:15 +0300

    PLAT-522, PLAT-521, PLAT-586: add test for double pulse change (#424)
    * PLAT-521: add test
    
    Co-authored-by: mariavasilenko <maria.vasilenko@insolar.io>
    Co-authored-by: Eugene Blikh <bigbes@gmail.com>

--- ledger-core/virtual/execute/execute.go
+++ ledger-core/virtual/execute/execute.go
@@ -484,10 +484,10 @@ func (s *SMExecute) stepExecuteStart(ctx smachine.ExecutionContext) smachine.Sta
 		if s.executionNewState == nil {
 			panic(throw.IllegalValue())
 		}
-	}).DelayedStart().ThenJump(s.stepWaitExecutionResult)
+	}).DelayedStart().ThenJump(s.StepWaitExecutionResult)
 }
 
-func (s *SMExecute) stepWaitExecutionResult(ctx smachine.ExecutionContext) smachine.StateUpdate {
+func (s *SMExecute) StepWaitExecutionResult(ctx smachine.ExecutionContext) smachine.StateUpdate {
 	if s.executionNewState == nil {
 		return ctx.Sleep().ThenRepeat()
 	}
@@ -663,7 +663,7 @@ func (s *SMExecute) stepExecuteContinue(ctx smachine.ExecutionContext) smachine.
 		if s.executionNewState == nil {
 			panic(throw.IllegalState())
 		}
-	}).DelayedStart().ThenJump(s.stepWaitExecutionResult)
+	}).DelayedStart().ThenJump(s.StepWaitExecutionResult)
 }
 
 func (s *SMExecute) stepSaveNewObject(ctx smachine.ExecutionContext) smachine.StateUpdate {
@@ -673,6 +673,10 @@ func (s *SMExecute) stepSaveNewObject(ctx smachine.ExecutionContext) smachine.St
 		memory []byte
 		class  reference.Global
 	)
+
+	if s.intolerableCall() {
+		s.executionNewState.Result = requestresult.New(executionNewState.Result(), executionNewState.ObjectReference())
+	}
 
 	if s.deactivate {
 		oldRequestResult := s.executionNewState.Result
