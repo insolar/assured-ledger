41769b44815c53acb6d5aedf50e8889bf6b6a231
Red Cyrax <cyraxred@users.noreply.github.com>
2020-07-03 22:56:16 +0300

    Further work

---
+++ ledger-core/ledger/server/catalog/entry.go
@@ -0,0 +1,44 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package catalog
+
+import (
+	"github.com/insolar/assured-ledger/ledger-core/rms"
+)
+
+type Entry = rms.CatalogEntry
+type Ordinal uint32
+type ExtensionID uint32
+
+type SectionEntry struct {
+	ExtensionID
+	Data []byte
+}
+
+type WriteEntry struct {
+	Entry
+	Body       SectionEntry
+	Payload    SectionEntry
+	Extensions []SectionEntry
+}
+
+type WriteBundle struct {
+	Entries    []WriteEntry
+	CallbackFn func(WrittenBundle)
+}
+
+type WrittenEntry struct {
+	EntryLoc   StorageLocator
+	BodyLoc    StorageLocator
+	PayloadLoc StorageLocator
+	ExtLocs    []StorageLocator
+}
+
+type WrittenBundle struct {
+	First   Ordinal
+	Entries []WrittenEntry
+}
+
