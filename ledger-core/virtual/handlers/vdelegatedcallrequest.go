9cefce73e9cff19ad929d44732031a67b874355d
ivanshibitov <ivan.shibitov@insolar.io>
2020-07-07 11:52:25 +0300

    Plat-536: fix process message in past pulse (#432)

--- ledger-core/virtual/handlers/vdelegatedcallrequest.go
+++ ledger-core/virtual/handlers/vdelegatedcallrequest.go
@@ -66,7 +66,18 @@ func (s *SMVDelegatedCallRequest) GetStateMachineDeclaration() smachine.StateMac
 }
 
 func (s *SMVDelegatedCallRequest) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
+	if s.pulseSlot.State() != conveyor.Present {
+		ctx.Log().Trace("stop processing VDelegatedCallRequest since we are not in present pulse")
+		return ctx.Stop()
+	}
+
+	ctx.SetDefaultMigration(s.migrationDefault)
 	return ctx.Jump(s.stepProcess)
+}
+
+func (s *SMVDelegatedCallRequest) migrationDefault(ctx smachine.MigrationContext) smachine.StateUpdate {
+	ctx.Log().Trace("stop processing VDelegatedCallRequest since pulse was changed")
+	return ctx.Stop()
 }
 
 func (s *SMVDelegatedCallRequest) stepProcess(ctx smachine.ExecutionContext) smachine.StateUpdate {
