78ded6d016ec61b548ae39b5f154d07a43b624de
Red Cyrax <cyraxred@users.noreply.github.com>
2020-07-13 18:58:43 +0300

    Further work

--- ledger-core/rms/proto_gen.go
+++ ledger-core/rms/proto_gen.go
@@ -7,4 +7,4 @@ package rms
 
 // `go:generate` command below is here just for information. It should not be used by go generate.
 // Actual generation is in the Makefile.
-//go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
+// go:generate protoc -I=. -I=$GOPATH/src --ins_out=./ proto_*.proto
