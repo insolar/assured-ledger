71712a762d4766e65f359477a3e3066226955784
mariavasilenko <maria.vasilenko@insolar.io>
2020-07-14 17:37:40 +0300

    PLAT-559:  add test

---
+++ ledger-core/virtual/integration/sender_test.go
@@ -0,0 +1,134 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package integration
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/insolar/assured-ledger/ledger-core/insolar/jet"
+	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
+	"github.com/insolar/assured-ledger/ledger-core/instrumentation/inslogger"
+	"github.com/insolar/assured-ledger/ledger-core/reference"
+	"github.com/insolar/assured-ledger/ledger-core/virtual/authentication"
+	"github.com/insolar/assured-ledger/ledger-core/virtual/integration/utils"
+)
+
+var messagesWithoutToken = []struct {
+	name  string
+	msg   interface{}
+	pulse []string
+}{
+	{
+		name:  "VCallRequest",
+		msg:   &payload.VCallRequest{},
+		pulse: []string{"P"},
+	},
+	// {
+	// 	name:  "VCallResult",
+	// 	msg:   &payload.VCallResult{},
+	// 	pulse: []string{"P", "P-1", "any"},
+	// },
+	// {
+	// 	name:  "VStateReport",
+	// 	msg:   &payload.VStateReport{},
+	// 	pulse: []string{"P-1", "any"},
+	// },
+	// {
+	// 	name:  "VStateRequest",
+	// 	msg:   &payload.VStateRequest{},
+	// 	pulse: []string{"P", "P-1", "any"},
+	// },
+	// {
+	// 	name:  "VDelegatedCallRequest",
+	// 	msg:   &payload.VDelegatedCallRequest{},
+	// 	pulse: []string{"P-1", "any"},
+	// },
+	// {
+	// 	name:  "VFindCallRequest",
+	// 	msg:   &payload.VDelegatedCallResponse{},
+	// 	pulse: []string{"P"},
+	// },
+	// {
+	// 	name:  "VDelegatedRequestFinished",
+	// 	msg:   &payload.VDelegatedRequestFinished{},
+	// 	pulse: []string{"P", "P-1", "any"},
+	// },
+	// {
+	// 	name:  "VFindCallResponse",
+	// 	msg:   &payload.VFindCallResponse{},
+	// 	pulse: []string{"P-1", "any"},
+	// },
+	// {
+	// 	name:  "VFindCallRequest",
+	// 	msg:   &payload.VFindCallRequest{},
+	// 	pulse: []string{"P"},
+	// },
+}
+
+func TestSender_SuccessChecks(t *testing.T) {
+	t.Log("C5188")
+
+	for _, testMsg := range messagesWithoutToken {
+		t.Run(testMsg.name, func(t *testing.T) {
+			for _, p := range testMsg.pulse {
+				mc := minimock.NewController(t)
+
+				server, ctx := utils.NewUninitializedServer(nil, t)
+
+				jetCoordinatorMock := jet.NewAffinityHelperMock(mc)
+				auth := authentication.NewService(ctx, jetCoordinatorMock)
+				server.ReplaceAuthenticationService(auth)
+
+				var errorFound bool
+				logHandler := func(arg interface{}) {
+					err, ok := arg.(error)
+					if !ok {
+						return
+					}
+					errMsg := err.Error()
+					if !strings.Contains(errMsg, "illegitimate msg") {
+						return
+					}
+					errorFound = true
+				}
+				logger := utils.InterceptLog(inslogger.FromContext(ctx), logHandler)
+				server.OverrideConveyorFactoryLogContext(inslogger.SetLogger(ctx, logger))
+
+				server.Init(ctx)
+				server.IncrementPulseAndWaitIdle(ctx)
+
+				sender := server.RandomGlobalWithPulse()
+				switch p {
+				case "P":
+				case "P-1":
+					server.IncrementPulse(ctx)
+				case "any":
+					server.IncrementPulse(ctx)
+					server.IncrementPulse(ctx)
+				default:
+					t.Fatal("unexpected")
+				}
+
+				jetCoordinatorMock.
+					MeMock.Return(server.RandomGlobalWithPulse()).
+					QueryRoleMock.Return([]reference.Global{sender}, nil)
+
+				msg := server.WrapPayload(testMsg.msg.(payload.Marshaler)).SetSender(sender).Finalize()
+				server.SendMessage(ctx, msg)
+
+				server.WaitActiveThenIdleConveyor()
+
+				assert.False(t, errorFound, "Fail "+testMsg.name+", pulse = "+p)
+				server.Stop()
+				mc.Finish()
+			}
+		})
+	}
+}
