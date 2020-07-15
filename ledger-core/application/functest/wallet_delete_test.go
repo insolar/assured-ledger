c6ca9e5c4c1ecf02f3cc79f2a3458cc625523df7
Ekaterina Rogushkova <catherine.zamaraeva@gmail.com>
2020-07-15 17:21:56 +0300

    PLAT-415: add simple functest for wallet/delete

---
+++ ledger-core/application/functest/wallet_delete_test.go
@@ -0,0 +1,38 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+// +build functest
+
+package functest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Creates wallet, calls /wallet/delete, then calls /wallet/get_balance and gets error.
+func TestWalletDelete(t *testing.T) {
+	t.Log("C4859")
+	walletRef, err := createSimpleWallet()
+	require.NoError(t, err, "failed to create wallet")
+
+	deleteURL := getURL(walletDeletePath, "", "")
+	rawResp, err := sendAPIRequest(deleteURL, walletDeleteRequestBody{Ref: walletRef})
+	require.NoError(t, err, "failed to send request or get response body")
+
+	resp, err := unmarshalWalletDeleteResponse(rawResp)
+	require.NoError(t, err, "failed to unmarshal response")
+
+	require.Empty(t, resp.Err, "problem during execute request")
+	assert.NotEmpty(t, resp.TraceID, "traceID mustn't be empty")
+
+	// TODO: unskip after realization
+	// getBalanceURL := getURL(walletGetBalancePath, "", "")
+	// _, getBalanceErr := getWalletBalance(getBalanceURL, walletRef)
+	// require.Error(t, getBalanceErr)
+	// require.Contains(t, getBalanceErr, "todo")
+}
