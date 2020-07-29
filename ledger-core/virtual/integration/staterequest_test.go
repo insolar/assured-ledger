f47c414d133edbc0933ddf3135582cc28241b226
mariavasilenko <maria.vasilenko@insolar.io>
2020-07-29 15:31:37 +0300

    PLAT-678: add test 2

--- ledger-core/virtual/integration/staterequest_test.go
+++ ledger-core/virtual/integration/staterequest_test.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/insolar/assured-ledger/ledger-core/application/builtin/proxy/testwallet"
 	"github.com/insolar/assured-ledger/ledger-core/insolar"
+	"github.com/insolar/assured-ledger/ledger-core/insolar/contract"
 	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
 	"github.com/insolar/assured-ledger/ledger-core/pulse"
 	"github.com/insolar/assured-ledger/ledger-core/reference"
@@ -218,7 +219,6 @@ func TestVirtual_VStateRequest_WhenObjectIsDeactivated(t *testing.T) {
 				validatedStateRef = server.RandomLocalWithPulse()
 				pulseNumber       = server.GetPulse().PulseNumber
 			)
-			server.IncrementPulse(ctx)
 
 			typedChecker := server.PublisherMock.SetTypedChecker(ctx, mc, server)
 			// todo
@@ -245,6 +245,7 @@ func TestVirtual_VStateRequest_WhenObjectIsDeactivated(t *testing.T) {
 				}
 
 				pl := &payload.VStateReport{
+					AsOf:            pulseNumber,
 					Status:          payload.Inactive,
 					Object:          objectGlobal,
 					ProvidedContent: content,
@@ -268,6 +269,99 @@ func TestVirtual_VStateRequest_WhenObjectIsDeactivated(t *testing.T) {
 			testutils.WaitSignalsTimed(t, 10*time.Second, server.Journal.WaitAllAsyncCallsDone())
 
 			assert.Equal(t, 1, typedChecker.VStateReport.Count())
+		})
+	}
+}
+
+func TestVirtual_CallMethod_WhenObjectIsDeactivated(t *testing.T) {
+	t.Log("C5474") // todo
+	table := []struct {
+		name          string
+		requestState  contract.StateFlag
+		expectedError bool
+	}{
+		{name: "Request_State = dirty",
+			requestState:  contract.CallDirty,
+			expectedError: true},
+		{name: "Request_State = validated",
+			requestState:  contract.CallValidated,
+			expectedError: false,
+		},
+	}
+
+	for _, test := range table {
+		t.Run(test.name, func(t *testing.T) {
+			defer commontestutils.LeakTester(t)
+
+			mc := minimock.NewController(t)
+
+			server, ctx := utils.NewServerWithErrorFilter(nil, t, func(s string) bool {
+				return !strings.Contains(s, "(*SMExecute).stepSaveNewObject")
+			})
+			defer server.Stop()
+
+			var (
+				class        = testwallet.GetClass()
+				objectGlobal = reference.NewSelf(server.RandomLocalWithPulse())
+
+				dirtyStateRef     = server.RandomLocalWithPulse()
+				validatedStateRef = server.RandomLocalWithPulse()
+				pulseNumber       = server.GetPulse().PulseNumber
+			)
+
+			typedChecker := server.PublisherMock.SetTypedChecker(ctx, mc, server)
+			typedChecker.VCallResult.Set(func(result *payload.VCallResult) bool {
+
+				return false
+			})
+			// todo
+
+			// Send VStateReport with Dirty, Validated states
+			{
+				validatedState := payload.Ready
+				dirtyState := payload.Inactive
+
+				content := &payload.VStateReport_ProvidedContentBody{
+					LatestDirtyState: &payload.ObjectState{
+						Reference: dirtyStateRef,
+						Class:     class,
+						State:     insolar.MustSerialize(dirtyState),
+					},
+					LatestValidatedState: &payload.ObjectState{
+						Reference: validatedStateRef,
+						Class:     class,
+						State:     insolar.MustSerialize(validatedState),
+					},
+				}
+
+				pl := &payload.VStateReport{
+					AsOf:            pulseNumber,
+					Status:          payload.Inactive,
+					Object:          objectGlobal,
+					ProvidedContent: content,
+				}
+				server.SendPayload(ctx, pl)
+				testutils.WaitSignalsTimed(t, 10*time.Second, server.Journal.WaitStopOf(&handlers.SMVStateReport{}, 1))
+			}
+
+			server.IncrementPulse(ctx)
+
+			{
+				payload := &payload.VCallRequest{
+					CallType:            payload.CTMethod,
+					CallFlags:           payload.BuildCallFlags(contract.CallIntolerable, test.requestState),
+					Caller:              server.GlobalCaller(),
+					Callee:              objectGlobal,
+					CallSiteDeclaration: class,
+					CallSiteMethod:      "GetBalance",
+					CallOutgoing:        server.BuildRandomOutgoingWithPulse(),
+				}
+				server.SendPayload(ctx, payload)
+			}
+
+			testutils.WaitSignalsTimed(t, 10*time.Second, server.Journal.WaitAllAsyncCallsDone())
+
+			assert.Equal(t, 1, typedChecker.VCallResult.Count())
 		})
 	}
 }
