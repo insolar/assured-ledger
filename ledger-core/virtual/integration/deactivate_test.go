e2ba7bb972b5f9e0c6eb5e4f353715a9d8906405
Eugene Blikh <bigbes@gmail.com>
2020-08-06 20:29:22 +0400

    fix new test

--- ledger-core/virtual/integration/deactivate_test.go
+++ ledger-core/virtual/integration/deactivate_test.go
@@ -893,10 +893,11 @@ func TestVirtual_DeactivateObject_FinishPartialDeactivation(t *testing.T) {
 					State:        contract.CallDirty,
 				}
 				stateRef = server.RandomGlobalWithPulse()
+				outgoing = server.BuildRandomOutgoingWithPulse()
+				incoming = reference.NewRecordOf(objectRef, outgoing.GetLocal())
 			)
 
 			server.IncrementPulseAndWaitIdle(ctx)
-			outgoing := server.BuildRandomOutgoingWithPulse()
 			checkOutgoing := server.BuildRandomOutgoingWithPulse()
 
 			{
@@ -945,8 +946,10 @@ func TestVirtual_DeactivateObject_FinishPartialDeactivation(t *testing.T) {
 
 				// send delegation request finished with deactivate flag
 				pl := payload.VDelegatedRequestFinished{
+					CallType:     payload.CTMethod,
 					Callee:       objectRef,
 					CallOutgoing: outgoing,
+					CallIncoming: incoming,
 					CallFlags:    payload.BuildCallFlags(deactivateIsolation.Interference, deactivateIsolation.State),
 					LatestState: &payload.ObjectState{
 						State:       nil,
