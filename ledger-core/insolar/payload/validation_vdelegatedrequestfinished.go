d89222cc66330ccfef13fca33388464695c5d6bc
Eugene Blikh <bigbes@gmail.com>
2020-08-06 16:59:00 +0400

    fixed CR

--- ledger-core/insolar/payload/validation_vdelegatedrequestfinished.go
+++ ledger-core/insolar/payload/validation_vdelegatedrequestfinished.go
@@ -8,6 +8,7 @@ package payload
 import (
 	"github.com/insolar/assured-ledger/ledger-core/insolar/contract"
 	"github.com/insolar/assured-ledger/ledger-core/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/reference"
 	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
 
@@ -27,6 +28,22 @@ func (m *VDelegatedRequestFinished) isIntolerable() bool {
 	return m.GetCallFlags().GetInterference() == contract.CallIntolerable
 }
 
+func isTimePulseBefore(pn pulse.Number, before pulse.Number) bool {
+	return pn.IsTimePulse() && pn.IsBefore(before)
+}
+
+func isSpecialTimePulseBefore(pn pulse.Number, before pulse.Number) bool {
+	return pn.IsSpecial() || pn.IsTimePulse() && pn.IsBefore(before)
+}
+
+func globalBasePulseBeforeOrEqLocalPulse(global reference.Global) bool {
+	var (
+		basePulse  = global.GetBase().GetPulseNumber()
+		localPulse = global.GetLocal().GetPulseNumber()
+	)
+	return basePulse.IsBeforeOrEq(localPulse)
+}
+
 func (m *VDelegatedRequestFinished) Validate(currentPulse pulse.Number) error {
 	if err := m.validateUnimplemented(); err != nil {
 		return err
@@ -42,49 +59,64 @@ func (m *VDelegatedRequestFinished) Validate(currentPulse pulse.Number) error {
 		return throw.New("CallType must be valid")
 	}
 
-	callFlags := m.GetCallFlags()
-	if f := callFlags.GetState(); f >= contract.StateFlagCount || f <= contract.StateInvalid {
-		return throw.New("CallFlags state should be valid")
-	} else if f := callFlags.GetInterference(); f >= contract.InterferenceFlagCount || f <= contract.InterferenceInvalid {
-		return throw.New("CallFlags interference should be valid")
+	if !m.GetCallFlags().IsValid() {
+		return throw.New("CallFlags should be valid")
 	}
 
 	if !m.Callee.IsSelfScope() {
 		return throw.New("Callee must be valid self scoped Reference")
-	} else if pn := m.Callee.GetLocal().Pulse(); !pn.IsTimePulse() || pn >= currentPulse {
+	}
+
+	var (
+		calleePulse = m.Callee.GetLocal().GetPulseNumber()
+	)
+	if !calleePulse.IsTimePulse() || !calleePulse.IsBefore(currentPulse) {
 		return throw.New("Callee must have valid time pulse with pulse lesser than current pulse")
 	}
 
 	if m.CallIncoming.IsEmpty() {
 		return throw.New("CallIncoming should be non-empty")
-	} else if pn := m.CallIncoming.GetLocal().Pulse(); !pn.IsTimePulse() || pn >= currentPulse {
+	}
+
+	var (
+		incomingLocalPulse = m.CallIncoming.GetLocal().GetPulseNumber()
+		incomingBasePulse  = m.CallIncoming.GetBase().GetPulseNumber()
+	)
+	switch {
+	case !isTimePulseBefore(incomingLocalPulse, currentPulse):
 		return throw.New("CallIncoming local part should have valid time pulse lesser than current pulse")
-	} else if pn := m.CallIncoming.GetBase().Pulse(); !pn.IsTimePulse() || pn >= currentPulse {
+	case !isTimePulseBefore(incomingBasePulse, currentPulse):
 		return throw.New("CallIncoming base part should have valid time pulse lesser than current pulse")
-	} else if m.CallIncoming.GetBase().Pulse() > m.CallIncoming.GetLocal().Pulse() {
+	case !globalBasePulseBeforeOrEqLocalPulse(m.CallIncoming):
 		return throw.New("CallIncoming base pulse should be less or equal than local pulse")
 	}
 
 	if m.CallOutgoing.IsEmpty() {
 		return throw.New("CallOutgoing should be non-empty")
-	} else if pn := m.CallOutgoing.GetLocal().Pulse(); !pn.IsTimePulse() || pn >= currentPulse {
+	}
+
+	var (
+		outgoingLocalPulse = m.CallOutgoing.GetLocal().GetPulseNumber()
+		outgoingBasePulse  = m.CallOutgoing.GetBase().GetPulseNumber()
+	)
+	switch {
+	case !isTimePulseBefore(outgoingLocalPulse, currentPulse):
 		return throw.New("CallOutgoing local part should have valid time pulse lesser than current pulse")
-	} else if pn := m.CallOutgoing.GetBase().Pulse(); !pn.IsSpecialOrTimePulse() || pn >= currentPulse {
+	case !isSpecialTimePulseBefore(outgoingBasePulse, currentPulse):
 		// probably call outgoing base part can be special (API Call)
 		return throw.New("CallOutgoing base part should have valid pulse lesser than current pulse")
-	} else if m.CallOutgoing.GetBase().Pulse() > m.CallOutgoing.GetLocal().Pulse() {
-		return throw.New("CallIncoming base pulse should be less or equal than local pulse")
+	case !globalBasePulseBeforeOrEqLocalPulse(m.CallOutgoing):
+		return throw.New("CallOutgoing base pulse should be less or equal than local pulse")
 	}
 
-	if m.CallOutgoing.GetLocal().Pulse() < m.CallIncoming.GetLocal().Pulse() {
+	switch {
+	case !outgoingLocalPulse.IsEqOrAfter(incomingLocalPulse):
 		return throw.New("CallOutgoing pulse should be more or equal than CallIncoming pulse")
-	} else if m.Callee.GetLocal().Pulse() > m.CallOutgoing.GetLocal().Pulse() {
+	case !incomingLocalPulse.IsEqOrAfter(calleePulse):
 		return throw.New("Callee pulse should be less or equal than CallOutgoing pulse")
-	}
-
-	if m.isIntolerable() && m.LatestState != nil {
+	case m.isIntolerable() && m.LatestState != nil:
 		return throw.New("LatestState should be empty on Intolerable call")
-	} else if m.CallType == CTConstructor && m.LatestState == nil {
+	case m.CallType == CTConstructor && m.LatestState == nil:
 		return throw.New("LatestState should be non-empty on Constructor call")
 	}
 
