9ee93d95529636e97ec4188ad3728d6385e5ac59
Red Cyrax <cyraxred@users.noreply.github.com>
2020-08-07 13:06:32 +0300

    Further work

--- ledger-core/ledger/server/lmnapp/lmn_app_test.go
+++ ledger-core/ledger/server/lmnapp/lmn_app_test.go
@@ -6,78 +6,26 @@
 package lmnapp
 
 import (
-	"context"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/insolar/assured-ledger/ledger-core/appctl/beat"
-	"github.com/insolar/assured-ledger/ledger-core/configuration"
-	"github.com/insolar/assured-ledger/ledger-core/conveyor/managed"
-	"github.com/insolar/assured-ledger/ledger-core/instrumentation/insapp"
 	"github.com/insolar/assured-ledger/ledger-core/instrumentation/insconveyor"
-	"github.com/insolar/assured-ledger/ledger-core/instrumentation/inslogger/instestlogger"
-	"github.com/insolar/assured-ledger/ledger-core/network/messagesender"
-	"github.com/insolar/assured-ledger/ledger-core/testutils"
-	"github.com/insolar/assured-ledger/ledger-core/testutils/gen"
-	"github.com/insolar/assured-ledger/ledger-core/testutils/testpop"
 )
 
 func TestStartCtm(t *testing.T) {
-	instestlogger.SetTestOutput(t)
+	server := NewTestServer(t)
+	defer server.Stop()
 
-	// Reuse default compartment construction logic
-	app := NewAppCompartment(configuration.Ledger{}, insapp.AppComponents{
-		// Provide mocks for application-level / cross-compartment dependencies here
-		MessageSender: messagesender.NewServiceMock(t),
+	// optional
+	server.SetImposer(func(params *insconveyor.ImposedParams) {
+		// impose per-test changes upon default behavior
 	})
-	require.NotNil(t, app)
+	server.Start()
 
-	app.SetImposer(func(params *insconveyor.ImposedParams) {
-		// impose upon default behavior
+	// do your test here
 
-		// params.CompartmentSetup.ConveyorConfig allows to adjust conveyor's settings
-		params.CompartmentSetup.ConveyorConfig.EventlessSleep = 0
-
-		//	params.CompartmentSetup.EventFactoryFn - override event factory
-		//	params.CompartmentSetup.ConveyorCycleFn - set cycle fn to control worker's stepping behavior
-
-		// params.CompartmentSetup.Components - to add/remove/replace conveyor-depended components
-
-		// params.CompartmentSetup.Dependencies - to add/remove/replace dependencies provided to SMs
-		params.CompartmentSetup.Dependencies.DeleteDependency("something")
-
-		// params.ComponentInterceptFn can be used to prevent or to replace any of
-		// conveyor-depended components.
-		// For now this is similar to do changes for params.CompartmentSetup.Components
-		params.ComponentInterceptFn = func(c managed.Component) managed.Component {
-			return c
-		}
-	})
-
-	ctx := context.Background()
-
-	err := app.Init(ctx)
-	require.NoError(t, err)
-
-	err = app.Start(ctx)
-	require.NoError(t, err)
-
-	localRef := gen.UniqueGlobalRef()
-	pg := testutils.NewPulseGenerator(10, testpop.CreateOneNodePopulationMock(t, localRef))
-	pg.Generate()
-
-	bd := app.GetBeatDispatcher()
 	for i := 3; i > 0; i-- {
 		time.Sleep(10*time.Millisecond)
-		pg.Generate()
-
-		ack, _ := beat.NewAck(func(beat.AckData) {})
-		bd.PrepareBeat(ack)
-		bd.CommitBeat(pg.GetLastBeat())
+		server.NextPulse()
 	}
-
-	err = app.Stop(ctx)
-	require.NoError(t, err)
 }
