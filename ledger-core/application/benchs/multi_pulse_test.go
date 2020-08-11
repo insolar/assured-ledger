250c2861c52cfb127293e06805e5ac6a57a42c29
Ilya Ozherelyev <ilya.ozherelyev@insolar.io>
2020-08-12 01:20:52 +0300

    refactor a bit

--- ledger-core/application/benchs/multi_pulse_test.go
+++ ledger-core/application/benchs/multi_pulse_test.go
@@ -6,7 +6,6 @@
 package benchs
 
 import (
-	"context"
 	"fmt"
 	"testing"
 
@@ -17,34 +16,10 @@ import (
 func BenchmarkMultiPulse(b *testing.B) {
 	instestlogger.SetTestOutput(b)
 
-	ctx := context.Background()
 	for numNodes := 2; numNodes <= 5; numNodes++ {
 		b.Run(fmt.Sprintf("Nodes %d", numNodes), func(b *testing.B) {
 
-			res := launchnet.CustomRunWithPulsar(numNodes, 0, 0, func(apiAddresses []string) int {
-				setAPIAddresses(apiAddresses)
-
-				numWallets := numNodes * 10
-
-				wallets := make([]string, 0, numWallets)
-				for i := 0; i < numWallets; i++ {
-					wallet, err := createSimpleWallet(ctx)
-					if err != nil {
-						return 2
-					}
-					wallets = append(wallets, wallet)
-				}
-
-				res := runGetBench(wallets)
-				if res != nil {
-					return 2
-				}
-				res = runSetBench(wallets)
-				if res != nil {
-					return 2
-				}
-				return 0
-			})
+			res := launchnet.CustomRunWithPulsar(numNodes, 0, 0, runBenchOnNetwork(numNodes))
 
 			if res != 0 {
 				b.Error("network run failed")
--- ledger-core/application/benchs/single_pulse_test.go
+++ ledger-core/application/benchs/single_pulse_test.go
@@ -22,40 +22,42 @@ import (
 func BenchmarkSinglePulse(b *testing.B) {
 	instestlogger.SetTestOutput(b)
 
-	ctx := context.Background()
 	for numNodes := 2; numNodes <= 5; numNodes++ {
 		b.Run(fmt.Sprintf("Nodes %d", numNodes), func(b *testing.B) {
-
-			res := launchnet.CustomRunWithoutPulsar(numNodes, 0, 0, func(apiAddresses []string) int {
-				setAPIAddresses(apiAddresses)
-
-				numWallets := numNodes * 1000
-
-				wallets := make([]string, 0, numWallets)
-				for i := 0; i < numWallets; i++ {
-					wallet, err := createSimpleWallet(ctx)
-					if err != nil {
-						return 2
-					}
-					wallets = append(wallets, wallet)
-				}
-
-				res := runGetBench(wallets)
-				if res != nil {
-					return 2
-				}
-				res = runSetBench(wallets)
-				if res != nil {
-					return 2
-				}
-				return 0
-			})
-
+			res := launchnet.CustomRunWithoutPulsar(numNodes, 0, 0, runBenchOnNetwork(numNodes))
 			if res != 0 {
 				b.Error("network run failed")
 				b.Fatal("failed test run")
 			}
 		})
+	}
+}
+
+func runBenchOnNetwork(numNodes int) func(apiAddresses []string) int {
+	return func(apiAddresses []string) int {
+		ctx := context.Background()
+		setAPIAddresses(apiAddresses)
+
+		numWallets := numNodes * 1000
+
+		wallets := make([]string, 0, numWallets)
+		for i := 0; i < numWallets; i++ {
+			wallet, err := createSimpleWallet(ctx)
+			if err != nil {
+				return 2
+			}
+			wallets = append(wallets, wallet)
+		}
+
+		res := runGetBench(wallets)
+		if res != nil {
+			return 2
+		}
+		res = runSetBench(wallets)
+		if res != nil {
+			return 2
+		}
+		return 0
 	}
 }
 
