26dea93717d3c9879ddc5a5b2485a979d481d576
Eugene Blikh <bigbes@gmail.com>
2020-08-20 14:52:56 +0300

    NOISSUE: make require to be great again in mocks
    require uses FailNow that uses runtime.GoExit() to stop goroutine
    use separate goroutine (with timeout) to handle that case correctly

--- ledger-core/virtual/integration/mock/publisher/checker/default.go
+++ ledger-core/virtual/integration/mock/publisher/checker/default.go
@@ -7,8 +7,12 @@ package checker
 
 import (
 	"context"
+	"time"
 
 	"github.com/ThreeDotsLabs/watermill/message"
+
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/synckit"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
 
 type Sender interface {
@@ -24,26 +28,43 @@ var _ Checker = &Default{}
 type CallbackFn func(topic string, messages ...*message.Message) error
 
 type Default struct {
+	timeout   time.Duration
 	checkerFn CallbackFn
 }
 
 func (p *Default) CheckMessages(topic string, messages ...*message.Message) error {
-	return p.checkerFn(topic, messages...)
+	var (
+		done = make(synckit.ClosableSignalChannel)
+		err  error
+	)
+
+	go func() {
+		defer func() { _ = synckit.SafeClose(done) }()
+
+		err = p.checkerFn(topic, messages...)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(p.timeout):
+		err = throw.New("timeout: failed to check message")
+	}
+
+	return err
 }
 
 func NewDefault(fn CallbackFn) *Default {
 	return &Default{
+		timeout:   10 * time.Second,
 		checkerFn: fn,
 	}
 }
 
 func NewResend(ctx context.Context, sender Sender) *Default {
-	return &Default{
-		checkerFn: func(topic string, messages ...*message.Message) error {
-			for _, msg := range messages {
-				sender.SendMessage(ctx, msg)
-			}
-			return nil
-		},
-	}
+	return NewDefault(func(topic string, messages ...*message.Message) error {
+		for _, msg := range messages {
+			sender.SendMessage(ctx, msg)
+		}
+		return nil
+	})
 }
