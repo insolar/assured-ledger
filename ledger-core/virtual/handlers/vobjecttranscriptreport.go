b9262f9f8e6f6cea5c987db7058937f20f0b90e8
Ruslan Zakirov <Ruslan.Zakirov@gmail.com>
2020-08-26 19:13:26 +0300

    PLAT-754: stub of VObjectTranscriptReport message (#707)
    stub to start with tests, details later

---
+++ ledger-core/virtual/handlers/vobjecttranscriptreport.go
@@ -0,0 +1,55 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+//go:generate sm-uml-gen -f $GOFILE
+
+package handlers
+
+import (
+	"github.com/insolar/assured-ledger/ledger-core/conveyor/smachine"
+	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/injector"
+)
+
+type SMVObjectTranscriptReport struct {
+	// input arguments
+	Meta    *payload.Meta
+	Payload *payload.VObjectTranscriptReport
+}
+
+/* -------- Declaration ------------- */
+
+var dSMVObjectTranscriptReportInstance smachine.StateMachineDeclaration = &dSMVObjectTranscriptReport{}
+
+type dSMVObjectTranscriptReport struct {
+	smachine.StateMachineDeclTemplate
+}
+
+func (*dSMVObjectTranscriptReport) InjectDependencies(sm smachine.StateMachine, _ smachine.SlotLink, injector injector.DependencyInjector) {
+	_ = sm.(*SMVObjectTranscriptReport)
+}
+
+func (*dSMVObjectTranscriptReport) GetInitStateFor(sm smachine.StateMachine) smachine.InitFunc {
+	s := sm.(*SMVObjectTranscriptReport)
+	return s.Init
+}
+
+/* -------- Instance ------------- */
+
+func (*SMVObjectTranscriptReport) InjectDependencies(sm smachine.StateMachine, _ smachine.SlotLink, injector *injector.DependencyInjector) {
+	_ = sm.(*SMVObjectTranscriptReport)
+}
+
+func (s *SMVObjectTranscriptReport) GetStateMachineDeclaration() smachine.StateMachineDeclaration {
+	return dSMVObjectTranscriptReportInstance
+}
+
+func (s *SMVObjectTranscriptReport) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
+	return ctx.Jump(s.stepProcess)
+}
+
+func (s *SMVObjectTranscriptReport) stepProcess(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	return ctx.Stop()
+}
