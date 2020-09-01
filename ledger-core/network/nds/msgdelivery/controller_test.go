16f7d0385f9a326d1db69cad43acd3596692ff6d
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2020-09-01 23:11:20 +0300

    PLAT-794: Rework controller_test.go

--- ledger-core/network/nds/msgdelivery/controller_test.go
+++ ledger-core/network/nds/msgdelivery/controller_test.go
@@ -7,40 +7,555 @@ package msgdelivery
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/insolar/assured-ledger/ledger-core/pulse"
-	"github.com/insolar/assured-ledger/ledger-core/vanilla/cryptkit"
-	"github.com/insolar/assured-ledger/ledger-core/vanilla/longbits"
-
 	"github.com/insolar/assured-ledger/ledger-core/network/nds/uniproto"
 	"github.com/insolar/assured-ledger/ledger-core/network/nds/uniproto/l2/uniserver"
 	"github.com/insolar/assured-ledger/ledger-core/network/nwapi"
+	"github.com/insolar/assured-ledger/ledger-core/pulse"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/cryptkit"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/longbits"
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/synckit"
 )
 
-func TestController(t *testing.T) {
+var noopReceiver = func(_ ReturnAddress, _ nwapi.PayloadCompleteness, _ interface{}) error {
+	return nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) []rune {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return b
+}
+
+func TestShipToHead(t *testing.T) {
+	payloadLen := 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+	}
+
+	var _, srv2 Service
+
+	ch1 := make(chan string, 1)
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch1 <- vo
+
+		return nil
+	}
+
+	_, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expPayload := head.S
+	actlPayload := <-ch1
+
+	require.Equal(t, expPayload, actlPayload)
+}
+
+func TestShipToBody(t *testing.T) {
+	payloadLen := 1024 * 1024 * 64
+
+	runes := RandStringRunes(payloadLen)
+
+	body := TestString{string(runes)}
+
+	sh := Shipment{
+		Body: &body,
+	}
+
+	var _, srv2 Service
+
+	ch1 := make(chan string, 1)
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch1 <- vo
+
+		return nil
+	}
+
+	_, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expPayload := body.S
+	actlPayload := <-ch1
+
+	require.Equal(t, expPayload, actlPayload)
+}
+
+func TestShipToHeadAndBody(t *testing.T) {
+	payloadLen := 1024 * 1024 * 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes[:64])}
+	body := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+		Body: &body,
+	}
+
+	var srv1, srv2 Service
+
+	ch1 := make(chan string, 2)
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.False(t, bool(done))
+
+		// Save received head
+		ch1 <- vo
+
+		err := srv1.PullBody(a, ShipmentRequest{
+			ReceiveFn: func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+				vo := v.(fmt.Stringer).String()
+				t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+				require.True(t, bool(done))
+
+				// Save received body
+				ch1 <- vo
+				return nil
+			},
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expHeadPayload := head.S
+	expBodyPayload := body.S
+	actlHeadPayload := <-ch1
+	actlBodyPayload := <-ch1
+
+	require.Equal(t, expHeadPayload, actlHeadPayload)
+	require.Equal(t, expBodyPayload, actlBodyPayload)
+}
+
+func TestEchoHead(t *testing.T) {
+	payloadLen := 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+	}
+
+	var srv1, srv2 Service
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.True(t, bool(done))
+
+		err := srv1.ShipReturn(a, Shipment{
+			Head: &TestString{S: vo + "echo1"},
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	ch2 := make(chan string, 1)
+
+	recv2 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-2:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch2 <- vo
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, recv2)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expPayload := head.S + "echo1"
+	actlPayload := <-ch2
+
+	require.Equal(t, expPayload, actlPayload)
+}
+
+func TestEchoHeadAndBody(t *testing.T) {
+	payloadLen := 1024 * 1024 * 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes[:64])}
+	body := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+		Body: &body,
+	}
+
+	var srv1, srv2 Service
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.False(t, bool(done))
+
+		echoHead := TestString{S: vo + "echo1"}
+
+		err := srv1.PullBody(a, ShipmentRequest{
+			ReceiveFn: func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+				vo := v.(fmt.Stringer).String()
+
+				require.True(t, bool(done))
+
+				err := srv1.ShipReturn(a, Shipment{
+					Head: &echoHead,
+					Body: &TestString{S: vo + "echo1"},
+				})
+
+				require.NoError(t, err)
+
+				return nil
+			},
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	ch2 := make(chan string, 2)
+
+	recv2 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-2:"), len(vo))
+
+		require.False(t, bool(done))
+
+		ch2 <- vo
+
+		err := srv2.PullBody(a, ShipmentRequest{
+			ReceiveFn: func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+				vo := v.(fmt.Stringer).String()
+				t.Log(a.String(), fmt.Sprintf("ctrl-2:"), len(vo))
+
+				require.True(t, bool(done))
+
+				ch2 <- vo
+
+				return nil
+			},
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, recv2)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expHeadPayload := head.S + "echo1"
+	expBodyPayload := body.S + "echo1"
+	actlHeadPayload := <-ch2
+	actlBodyPayload := <-ch2
+
+	require.Equal(t, expHeadPayload, actlHeadPayload)
+	require.Equal(t, expBodyPayload, actlBodyPayload)
+}
+
+func TestShipToCancel(t *testing.T) {
+	payloadLen := 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes)}
+
+	ch := synckit.NewChainedCancel()
+	sh := Shipment{
+		Head:   &head,
+		Cancel: ch,
+	}
+
+	ch.Cancel()
+
+	var _, srv2 Service
+
+	ch1 := make(chan string, 1)
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch1 <- vo
+
+		return nil
+	}
+
+	_, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	_, ok := <-ch1
+
+	require.False(t, ok)
+}
+
+func TestShipReturnCancel(t *testing.T) {
+	payloadLen := 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+	}
+
+	var srv1, srv2 Service
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch := synckit.NewChainedCancel()
+		ch.Cancel()
+
+		err := srv1.ShipReturn(a, Shipment{
+			Head:   &TestString{S: vo + "echo1"},
+			Cancel: ch,
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	ch2 := make(chan string, 1)
+
+	recv2 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-2:"), len(vo))
+
+		require.True(t, bool(done))
+
+		ch2 <- vo
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, recv2)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	_, ok := <-ch2
+
+	require.False(t, ok)
+}
+
+func TestPullBodyCancel(t *testing.T) {
+	t.Skip("")
+	payloadLen := 1024 * 1024 * 64
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes[:64])}
+	body := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+		Body: &body,
+	}
+
+	var srv1, srv2 Service
+
+	ch1 := make(chan string, 1)
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.False(t, bool(done))
+
+		ch := synckit.NewChainedCancel()
+		ch.Cancel()
+
+		err := srv1.PullBody(a, ShipmentRequest{
+			ReceiveFn: func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+				vo := v.(fmt.Stringer).String()
+				t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+				require.True(t, bool(done))
+
+				ch1 <- vo
+				return nil
+			},
+			Cancel: ch,
+		})
+
+		require.NoError(t, err)
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	_, ok := <-ch1
+
+	require.False(t, ok)
+}
+
+func TestRejectBody(t *testing.T) {
+	t.Skip("")
+	payloadLen := 1024 * 1024 * 512
+
+	runes := RandStringRunes(payloadLen)
+
+	head := TestString{string(runes[:64])}
+	body := TestString{string(runes)}
+
+	sh := Shipment{
+		Head: &head,
+		Body: &body,
+	}
+
+	var srv1, srv2 Service
+
+	ch1 := make(chan string, 2)
+
+	recv1 := func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+		vo := v.(fmt.Stringer).String()
+		t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+		require.False(t, bool(done))
+
+		err := srv1.PullBody(a, ShipmentRequest{
+			ReceiveFn: func(a ReturnAddress, done nwapi.PayloadCompleteness, v interface{}) error {
+				vo := v.(fmt.Stringer).String()
+				t.Log(a.String(), fmt.Sprintf("ctrl-1:"), len(vo))
+
+				require.True(t, bool(done))
+
+				ch1 <- vo
+				return nil
+			},
+		})
+		require.NoError(t, err)
+
+		err = srv1.RejectBody(a)
+		require.NoError(t, err)
+
+		err = srv1.PullBody(a, ShipmentRequest{
+			ReceiveFn: noopReceiver,
+		})
+
+		//TODO err maybe in ReceiveFn
+		require.Error(t, err)
+
+		// Save received head
+		ch1 <- vo
+
+		return nil
+	}
+
+	srv1, srv2, stop := startUniprotoServers(t, recv1, noopReceiver)
+	defer stop()
+
+	err := srv2.ShipTo(NewDirectAddress(1), sh)
+	require.NoError(t, err)
+
+	expHeadPayload := head.S
+	actlHeadPayload := <-ch1
+
+	require.Equal(t, expHeadPayload, actlHeadPayload)
+}
+
+func TestShipToWithTTL(t *testing.T) {
+
+}
+
+func TestShipReturnWithTTL(t *testing.T) {
+
+}
+
+func startUniprotoServers(t *testing.T, recv1, recv2 ReceiverFunc) (Service, Service, func()) {
 	const Server1 = "127.0.0.1:0"
 	const Server2 = "127.0.0.1:0"
 
 	vf := TestVerifierFactory{}
 	sk := cryptkit.NewSignatureKey(longbits.Zero(testDigestSize), testSignatureMethod, cryptkit.PublicAsymmetricKey)
 
-
-	var ctl1 Service
-	controller1 := NewController(Protocol, TestDeserializationFactory{},
-		func(a ReturnAddress, _ nwapi.PayloadCompleteness, v interface{}) error {
-			t.Log(a.String(), "Ctl1:", v)
-			s := v.(fmt.Stringer).String() + "-return"
-			return ctl1.ShipReturn(a, Shipment{Head: &TestString{s}})
-		}, nil, TestLogAdapter{t})
-
-	ctl1 = controller1.NewFacade()
+	ctrl1 := NewController(Protocol,
+		TestDeserializationFactory{},
+		recv1,
+		nil,
+		TestLogAdapter{t},
+	)
+	srv1 := ctrl1.NewFacade()
 
 	var dispatcher1 uniserver.Dispatcher
-	controller1.RegisterWith(dispatcher1.RegisterProtocol)
+	ctrl1.RegisterWith(dispatcher1.RegisterProtocol)
 
 	ups1 := uniserver.NewUnifiedServer(&dispatcher1, TestLogAdapter{t})
 	ups1.SetConfig(uniserver.ServerConfig{
@@ -69,18 +584,19 @@ func TestController(t *testing.T) {
 
 	/********************************/
 
-	results := make(chan string, 2)
-	controller2 := NewController(Protocol, TestDeserializationFactory{},
-		func(a ReturnAddress, _ nwapi.PayloadCompleteness, v interface{}) error {
-			s := v.(fmt.Stringer).String()
-			t.Log(a.String(), "Ctl2:", s)
-			results <- s
-			return nil
-		}, nil, TestLogAdapter{t})
+	ctrl2 := NewController(
+		Protocol,
+		TestDeserializationFactory{},
+		recv2,
+		nil,
+		TestLogAdapter{t},
+	)
+
+	srv2 := ctrl2.NewFacade()
 
 	var dispatcher2 uniserver.Dispatcher
 	dispatcher2.SetMode(uniproto.NewConnectionMode(0, Protocol))
-	controller2.RegisterWith(dispatcher2.RegisterProtocol)
+	ctrl2.RegisterWith(dispatcher2.RegisterProtocol)
 	dispatcher2.Seal()
 
 	ups2 := uniserver.NewUnifiedServer(&dispatcher2, TestLogAdapter{t})
@@ -110,20 +626,10 @@ func TestController(t *testing.T) {
 	require.NotNil(t, conn21)
 	require.NoError(t, conn21.Transport().EnsureConnect())
 
-	ctl2 := controller2.NewFacade()
-
-	// loopback
-	err = ctl2.ShipTo(NewDirectAddress(2), Shipment{Head: &TestString{"abc1"}})
 	require.NoError(t, err)
 
-	err = ctl2.ShipTo(NewDirectAddress(1), Shipment{Head: &TestString{"abc2"}})
-	require.NoError(t, err)
-
-	require.Equal(t, "abc1", <-results)
-	require.Equal(t, "abc2-return", <-results)
-
-	time.Sleep(controller1.timeCycle*10)
-
-	dispatcher2.Stop()
-	dispatcher1.Stop()
+	return srv1, srv2, func() {
+		dispatcher2.Stop()
+		dispatcher1.Stop()
+	}
 }
