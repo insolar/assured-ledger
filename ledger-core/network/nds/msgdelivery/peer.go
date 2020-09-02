8427768ff9d26c1f86334df14f8f06fbd46011f3
Red Cyrax <cyraxred@users.noreply.github.com>
2020-09-02 15:03:48 +0300

    Parcel TTL and PN

--- ledger-core/network/nds/msgdelivery/peer.go
+++ ledger-core/network/nds/msgdelivery/peer.go
@@ -168,21 +168,9 @@ func (p *DeliveryPeer) sendParcel(msg *msgShipment, isBody, isRepeated bool) {
 		ParcelType:   nwapi.CompletePayload,
 	}
 
-	cycle, pn := p.ctl.getPulseCycle()
-
-	if msg.expires < cycle {
+	if !p.applyParcelExpiry(msg, &packet) {
 		msg.markExpired()
 		return
-	}
-
-	if msg.shipment.Policies&ExactPulse == 0 {
-		packet.PulseNumber = pn
-		if cycle = msg.expires - cycle; cycle > math.MaxUint8 {
-			cycle = math.MaxUint8
-		}
-	} else {
-		packet.PulseNumber = msg.shipment.PN
-		cycle = uint32(msg.shipment.TTL)
 	}
 
 	switch {
@@ -198,20 +186,43 @@ func (p *DeliveryPeer) sendParcel(msg *msgShipment, isBody, isRepeated bool) {
 		packet.ParcelType = nwapi.HeadOnlyPayload
 		packet.Data = msg.shipment.Head
 		packet.BodyScale = uint8(bits.Len(msg.shipment.Body.ByteSize()))
-		packet.TTLCycles = uint8(cycle)
+		packet.TTLCycles = msg.shipment.TTL
 	}
 	p._sendParcel(uniproto.Any, packet, msg.canSendHead)
 }
 
+func (p *DeliveryPeer) applyParcelExpiry(msg *msgShipment, packet *ParcelPacket) bool {
+	cycle, pn := p.ctl.getPulseCycle()
+
+	switch {
+	case msg.expires < cycle:
+		return false
+
+	case msg.shipment.Policies&ExactPulse == 0:
+		msg.shipment.Policies |= ExactPulse
+		msg.shipment.PN = pn
+		if cycle = msg.expires - cycle; cycle > math.MaxUint8 {
+			msg.shipment.TTL = math.MaxUint8
+		} else {
+			msg.shipment.TTL = uint8(cycle)
+		}
+	}
+
+	packet.PulseNumber = msg.shipment.PN
+	return true
+}
+
 func (p *DeliveryPeer) sendLargeParcel(msg *msgShipment, isRepeated bool) {
-	_, pn := p.ctl.getPulseCycle()
-	//TODO how to correct calculate pn?
 	packet := ParcelPacket{
-		PulseNumber:  pn,
 		ParcelID:     msg.id.ShortID(),
 		ReturnID:     msg.returnID,
 		RepeatedSend: isRepeated,
 		ParcelType:   nwapi.CompletePayload,
+	}
+
+	if !p.applyParcelExpiry(msg, &packet) {
+		msg.markExpired()
+		return
 	}
 
 	packet.Data = msg.shipment.Body
