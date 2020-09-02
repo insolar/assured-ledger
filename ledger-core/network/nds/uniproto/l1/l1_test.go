1c1ab022aa8529247489c2c0a162df9dd2fa5aaa
Andrey Bronin <andrey.bronin@insolar.io>
2020-09-02 09:00:41 +0300

    unit tests

--- ledger-core/network/nds/uniproto/l1/l1_test.go
+++ ledger-core/network/nds/uniproto/l1/l1_test.go
@@ -20,14 +20,47 @@ const helloMsg = "hello"
 
 func TestNewTLS(t *testing.T) {
 
-	cer, err  := tls.LoadX509KeyPair("testdata/server.rsa.crt", "testdata/server.rsa.key")
+	cer, err := tls.LoadX509KeyPair("testdata/server.rsa.crt", "testdata/server.rsa.key")
 	require.NoError(t, err)
 
 	cfg := &tls.Config{Certificates: []tls.Certificate{cer}, InsecureSkipVerify: true}
 	assert.NotNil(t, cfg)
 	tp := NewTLS(nwapi.NewHostPort("127.0.0.1:0", true), nwapi.PreferV4, cfg)
+	defer tp.Close()
 
-	conns := make(chan(io.Reader))
+	conns := make(chan (io.Reader))
+	fn := func(local, remote nwapi.Address, conn io.ReadWriteCloser, w OneWayTransport, err error) (ok bool) {
+		conns <- conn
+		return true
+	}
+	outTransport, addr, err := tp.CreateListeningFactory(fn)
+	assert.NoError(t, err)
+	require.NotNil(t, outTransport)
+	defer outTransport.Close()
+	assert.False(t, addr.IsZero())
+
+	outTransport2, err := tp.CreateOutgoingOnlyFactory(fn)
+	assert.NoError(t, err)
+	require.NotNil(t, outTransport2)
+	defer outTransport2.Close()
+
+	transport, err := outTransport2.ConnectTo(addr)
+	assert.NoError(t, err)
+
+	err = transport.SendBytes([]byte(helloMsg))
+	assert.NoError(t, err)
+
+	buf := make([]byte, len(helloMsg))
+	_, err = io.ReadAtLeast(<-conns, buf, len(helloMsg))
+	assert.NoError(t, err)
+	assert.Equal(t, helloMsg, string(buf))
+}
+
+func TestNewTCP(t *testing.T) {
+	tp := NewTCP(nwapi.NewHostPort("127.0.0.1:0", true), nwapi.PreferV4)
+	defer tp.Close()
+
+	conns := make(chan (io.Reader))
 	fn := func(local, remote nwapi.Address, conn io.ReadWriteCloser, w OneWayTransport, err error) (ok bool) {
 		conns <- conn
 		return true
@@ -57,6 +90,8 @@ func TestNewTLS(t *testing.T) {
 
 func TestNewUDP(t *testing.T) {
 	tp := NewUDP(nwapi.NewHostPort("127.0.0.1:0", true), nwapi.PreferV4, MaxUDPSize)
+	defer tp.Close()
+
 	assert.Equal(t, MaxUDPSize, int(tp.MaxByteSize()))
 
 	fn := func(local, remote nwapi.Address, b []byte, err error) (ok bool) {
@@ -69,7 +104,6 @@ func TestNewUDP(t *testing.T) {
 	assert.False(t, addr.IsZero())
 	require.NotEqual(t, 0, addr.AsUDPAddr().Port)
 
-
 	outTransport2, err := tp.CreateOutgoingOnlyFactory()
 	assert.NoError(t, err)
 	require.NotNil(t, outTransport2)
@@ -81,4 +115,39 @@ func TestNewUDP(t *testing.T) {
 	err = transport.SendBytes([]byte(helloMsg))
 	assert.NoError(t, err)
 
+}
+
+func TestSessionfulTransportProvider_CreateListeningFactory_Negative(t *testing.T) {
+
+	cer, err := tls.LoadX509KeyPair("testdata/server.rsa.crt", "testdata/server.rsa.key")
+	require.NoError(t, err)
+
+	cfg := &tls.Config{Certificates: []tls.Certificate{cer}, InsecureSkipVerify: true}
+	assert.NotNil(t, cfg)
+
+	tests := []struct {
+		name     string
+		provider SessionfulTransportProvider
+		fn       SessionfulConnectFunc
+	}{
+		{
+			name:     "tcp",
+			provider: NewTCP(nwapi.NewHostPort("127.0.0.1:0", true), nwapi.PreferV4),
+			fn:       nil,
+		},
+		{
+			name:     "tls",
+			provider: NewTLS(nwapi.NewHostPort("127.0.0.1:0", true), nwapi.PreferV4, cfg),
+			fn:       nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Panics(t, func() {
+				_, _, _ = test.provider.CreateListeningFactory(test.fn)
+			})
+
+		})
+	}
 }
