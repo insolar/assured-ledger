0ef3596080e359db30357a44654f1a222eb8a69a
Andrey Bronin <andrey.bronin@insolar.io>
2020-09-03 14:17:04 +0300

    controller receiveParcel extra log

--- ledger-core/network/nds/msgdelivery/controller.go
+++ ledger-core/network/nds/msgdelivery/controller.go
@@ -7,6 +7,7 @@ package msgdelivery
 
 import (
 	"io"
+	"log"
 	"math"
 	"time"
 
@@ -278,6 +279,8 @@ func (p *Controller) receiveParcel(packet *uniproto.ReceivedPacket, payload *Par
 	}
 
 	duplicate := !dPeer.dedup.Add(DedupID(payload.ParcelID))
+	// todo: remove me
+	log.Printf("peerID: %d ParcelID: %d duplicate: %t", dPeer.peerID, payload.ParcelID, duplicate)
 
 	ok := false
 	if ok, _, retAddr.expires, err = p.adjustedExpiry(payload.PulseNumber, payload.TTLCycles, true); !ok {
