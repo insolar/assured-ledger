c19d3f4943e1c687ce0020bf6158c30fa3ebe585
Eugene Blikh <bigbes@gmail.com>
2020-09-08 16:27:34 +0300

    NOISSUE: more changes

--- ledger-core/virtual/execute/registeronlmn.go
+++ ledger-core/virtual/execute/registeronlmn.go
@@ -20,24 +20,22 @@ import (
 	"github.com/insolar/assured-ledger/ledger-core/reference"
 	"github.com/insolar/assured-ledger/ledger-core/rms"
 	"github.com/insolar/assured-ledger/ledger-core/runner/execution"
+	"github.com/insolar/assured-ledger/ledger-core/runner/requestresult"
+	"github.com/insolar/assured-ledger/ledger-core/testutils/gen"
 	"github.com/insolar/assured-ledger/ledger-core/vanilla/injector"
 	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
 )
 
-type RecordList []rms.BasicRecord
-
-func (c RecordList) Append(message rms.BasicRecord) RecordList {
-	return append(c, message)
+func mustRecordToAnyRecordLazy(rec rms.BasicRecord) rms.AnyRecordLazy {
+	rv := rms.AnyRecordLazy{}
+	if err := rv.SetAsLazy(rec); err != nil {
+		panic(err)
+	}
+	return rv
 }
 
-func (c RecordList) AsAnyRecordLazyList() ([]rms.AnyRecordLazy, error) {
-	rv := make([]rms.AnyRecordLazy, len(c))
-	for pos, record := range c {
-		if err := rv[pos].SetAsLazy(record); err != nil {
-			return nil, err
-		}
-	}
-	return rv, nil
+func isConstructor(request *payload.VCallRequest) bool {
+	return request.CallType == payload.CallTypeConstructor
 }
 
 // TODO: make all input arguments to be distinguishable from output arguments
@@ -45,20 +43,34 @@ func (c RecordList) AsAnyRecordLazyList() ([]rms.AnyRecordLazy, error) {
 // TODO: make all internal data to be separate
 type SMRegisterOnLMN struct {
 	// translate from VCallRequest.Payload. If set we need register incoming before any other registration
-	incoming rms.BasicRecord
+	// incoming rms.BasicRecord
+	//
+	// isConstructor bool
+	// object        reference.Global
+	// outgoing      reference.Global
+	// interference  isolation.InterferenceFlag
+	// newState      *execution.Update
+	//
+	// registeredOutbound bool
 
-	isConstructor bool
-	object        reference.Global
-	outgoing      reference.Global
-	interference  isolation.InterferenceFlag
-	newState      *execution.Update
+	// input arguments
+	Incoming       *payload.VCallRequest
+	Outgoing       *payload.VCallRequest
+	OutgoingResult *payload.VCallResult
+	IncomingResult *execution.Update
+	Interference   isolation.InterferenceFlag
 
-	registeredOutbound bool
-	messages           []payload.Marshaler
+	Object          reference.Global
+	LastFilamentRef reference.Global
+	LastLifelineRef reference.Global
 
-	// result flags show registration status
-	incomingRegistered bool
-	messagesRegistered bool
+	// internal data
+	messages []payload.Marshaler
+
+	// output arguments
+	NewObjectRef       reference.Global
+	NewLastFilamentRef reference.Global
+	NewLastLifelineRef reference.Global
 
 	// DI
 	messageSender messageSenderAdapter.MessageSender
@@ -91,6 +103,21 @@ func (s *SMRegisterOnLMN) GetStateMachineDeclaration() smachine.StateMachineDecl
 	return dSMRegisterOnLMNInstance
 }
 
+func (s *SMRegisterOnLMN) getRecordAnticipatedRef(object reference.Global, _ rms.BasicRecord) reference.Global {
+	var (
+		currentPulseNumber = s.pulseSlot.CurrentPulseNumber()
+		uniqueLocal        = gen.UniqueLocalRefWithPulse(currentPulseNumber)
+	)
+
+	if object.IsEmpty() {
+		return reference.NewSelf(uniqueLocal)
+	} else if !object.IsSelfScope() {
+		panic(throw.IllegalState())
+	}
+
+	return reference.NewRecordOf(object, uniqueLocal)
+}
+
 func (s *SMRegisterOnLMN) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
 	// possible variants here:
 	// all
@@ -101,146 +128,275 @@ func (s *SMRegisterOnLMN) Init(ctx smachine.InitializationContext) smachine.Stat
 	// Register *InboundResult* (all possible variants, different step, one message)
 	// Register (RInboundRequest + *InboundResult*) in two separate messages
 	switch {
-	case s.incoming != nil: // if we need to register incoming
+	case s.Incoming != nil && isConstructor(s.Incoming) && s.Object.IsEmpty(): // we need to register lifeline
+		return ctx.Jump(s.stepRegisterLifeline)
+	case s.Incoming != nil: // we need to register incoming
 		return ctx.Jump(s.stepRegisterIncoming)
-	// case -- if we need to register outgoing
-	case s.registeredOutbound: // if we need to register outgoing
+	case s.Outgoing != nil: // we need to register outgoing
+		return ctx.Jump(s.stepRegisterOutgoing)
+	case s.OutgoingResult != nil: // we need to register outgoing result
 		return ctx.Jump(s.stepRegisterOutgoingResult)
-	// case -- if we need to register incoming result
-	// case -- if we need to register outgoing result
-	default: // should be panic(throw.IllegalState) after all fixes
-		return ctx.Jump(s.stepRegisterRecords)
+	case s.IncomingResult != nil: // we need to register incoming result
+		return ctx.Jump(s.stepRegisterIncomingResult)
+	default:
+		panic(throw.IllegalState())
 	}
+}
+
+func (s *SMRegisterOnLMN) stepRegisterLifeline(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	if !s.Object.IsEmpty() {
+		panic(throw.IllegalValue())
+	} else if !s.LastFilamentRef.IsEmpty() {
+		panic(throw.IllegalValue())
+	}
+
+	var (
+		record         = &rms.RLifelineStart{}
+		anticipatedRef = s.getRecordAnticipatedRef(s.Object, record)
+	)
+
+	s.messages = append(s.messages, &rms.LRegisterRequest{
+		AnticipatedRef: rms.NewReference(anticipatedRef),
+		Flags:          rms.RegistrationFlags_FastSafe,
+		AnyRecordLazy:  mustRecordToAnyRecordLazy(record), // it should be based on
+		// TODO: here we should set all overrides, since RLifelineStart contains
+		//       ROutboundRequest and it has bad RootRef/PrevRef.
+		// OverrideRecordType: rms.RLifelineStart,
+		// OverridePrevRef:    NewReference(reference.Global{}), // must be empty
+		// OverrideRootRef:    NewReference(reference.Global{}), // must be empty
+		// OverrideReasonRef:  NewReference(<reference to outgoing>),
+	})
+
+	s.Object = anticipatedRef
+	s.LastFilamentRef = anticipatedRef
+
+	return ctx.Jump(s.stepSendMessages)
 }
 
 func (s *SMRegisterOnLMN) stepRegisterIncoming(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	records := RecordList{}
+	var record rms.BasicRecord
 
-	if s.isConstructor {
-		records = records.Append(&rms.RLifelineStart{})
+	switch s.Interference {
+	case isolation.CallTolerable:
+		if !s.LastLifelineRef.IsEmpty() {
+			panic(throw.IllegalState())
+		}
+		record = &rms.RLineInboundRequest{
+			RootRef: rms.NewReference(s.Object),
+			PrevRef: rms.NewReference(s.LastLifelineRef),
+		}
+	case isolation.CallIntolerable:
+		if !s.LastFilamentRef.IsEmpty() {
+			panic(throw.IllegalState())
+		}
+		record = &rms.RInboundRequest{
+			RootRef: rms.NewReference(s.Object),
+			PrevRef: rms.NewReference(s.LastFilamentRef),
+		}
 	}
 
-	records = records.Append(s.incoming)
+	anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
 
-	lazyRecords, err := records.AsAnyRecordLazyList()
-	if err != nil {
-		panic(err)
+	s.messages = append(s.messages, &rms.LRegisterRequest{
+		AnticipatedRef: rms.NewReference(anticipatedRef),
+		Flags:          rms.RegistrationFlags_FastSafe,
+		AnyRecordLazy:  mustRecordToAnyRecordLazy(record), // TODO: here we should provide record from incoming
+	})
+
+	switch s.Interference {
+	case isolation.CallTolerable:
+		s.LastLifelineRef = anticipatedRef
+	case isolation.CallIntolerable:
+		s.LastFilamentRef = anticipatedRef
 	}
 
-	req := &rms.LRegisterRequest{
-		Flags:   rms.RegistrationFlags_FastSafe,
-		Records: lazyRecords,
+	switch {
+	case s.Outgoing != nil:
+		return ctx.Jump(s.stepRegisterOutgoing)
+	case s.IncomingResult != nil:
+		return ctx.Jump(s.stepRegisterIncomingResult)
+	default:
+		panic(throw.Unsupported())
 	}
-
-	s.messages = append(s.messages, req)
-
-	return ctx.Jump(s.stepRegisterRecords)
-}
-
-func (s *SMRegisterOnLMN) stepRegisterRecords(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	if s.outgoing.IsEmpty() { // we should register result
-		return ctx.Jump(s.stepRegisterDone)
-	}
-
-	// we should register outgoing request
-	return ctx.Jump(s.stepRegisterOutgoing)
-}
-
-func (s *SMRegisterOnLMN) stepRegisterOutgoingResult(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	records := RecordList{&rms.ROutboundResponse{}}
-
-	lazyRecords, err := records.AsAnyRecordLazyList()
-	if err != nil {
-		panic(err)
-	}
-
-	req := &rms.LRegisterRequest{
-		Flags:   rms.RegistrationFlags_FastSafe,
-		Records: lazyRecords,
-	}
-
-	s.messages = append(s.messages, req)
-
-	return ctx.Jump(s.stepSendMessages)
 }
 
 func (s *SMRegisterOnLMN) stepRegisterOutgoing(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	records := RecordList{}
-
-	if s.registeredOutbound {
-	} else {
+	prevRef := s.LastFilamentRef
+	if prevRef.IsEmpty() {
+		prevRef = s.LastLifelineRef
+	}
+	if prevRef.IsEmpty() {
 		panic(throw.IllegalState())
 	}
 
-	switch s.interference {
-	case isolation.CallTolerable:
-		records = append(records, &rms.RLineInboundRequest{})
-	case isolation.CallIntolerable:
-		records = append(records, &rms.RInboundRequest{})
-	}
-	records = append(records, &rms.ROutboundRequest{})
-
-	lazyRecords := make([]rms.AnyRecordLazy, len(records))
-	for i, record := range records {
-		if err := lazyRecords[i].SetAsLazy(record); err != nil {
-			ctx.Log().Error("create lazyRecord fail", err)
-			panic(err)
-		}
+	record := &rms.ROutboundRequest{
+		RootRef: rms.NewReference(s.Object),
+		PrevRef: rms.NewReference(prevRef),
 	}
 
-	req := &rms.LRegisterRequest{
-		Flags:   rms.RegistrationFlags_FastSafe,
-		Records: lazyRecords,
-	}
+	anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
 
-	s.messages = append(s.messages, req)
+	s.messages = append(s.messages, &rms.LRegisterRequest{
+		AnticipatedRef: rms.NewReference(anticipatedRef),
+		Flags:          rms.RegistrationFlags_FastSafe,
+		AnyRecordLazy:  mustRecordToAnyRecordLazy(record), // TODO: here we should provide record from incoming
+	})
+
+	s.LastFilamentRef = anticipatedRef
 
 	return ctx.Jump(s.stepSendMessages)
 }
 
-func (s *SMRegisterOnLMN) stepRegisterDone(ctx smachine.ExecutionContext) smachine.StateUpdate {
-	records := make([]rms.BasicRecord, 0)
-	if s.registeredOutbound {
-		records = append(records, &rms.ROutboundResponse{})
-	} else if s.isConstructor {
-		records = append(records, &rms.RLineMemoryInit{})
-	}
-	records = append(records, &rms.RInboundResponse{})
-	if s.newState.Type == execution.Error {
-		records = append(records, &rms.RLineDeactivate{})
-	} else {
-		records = append(records, &rms.RLineMemory{})
-	}
-	if s.isConstructor {
-		records = append(records, &rms.RLineActivate{})
+func (s *SMRegisterOnLMN) stepRegisterOutgoingResult(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	if !s.LastFilamentRef.IsEmpty() {
+		panic(throw.IllegalState())
 	}
 
-	lazyRecords := make([]rms.AnyRecordLazy, len(records))
-	for i, record := range records {
-		if err := lazyRecords[i].SetAsLazy(record); err != nil {
-			ctx.Log().Error("create lazyRecord fail", err)
-			panic(err)
+	record := &rms.ROutboundResponse{
+		RootRef: rms.NewReference(s.Object),
+		PrevRef: rms.NewReference(s.LastFilamentRef),
+	}
+
+	anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
+
+	s.messages = append(s.messages, &rms.LRegisterRequest{
+		AnticipatedRef: rms.NewReference(anticipatedRef),
+		Flags:          rms.RegistrationFlags_FastSafe,
+		AnyRecordLazy:  mustRecordToAnyRecordLazy(record), // TODO: here we should provide record from incoming
+	})
+
+	s.LastFilamentRef = anticipatedRef
+
+	return ctx.Jump(s.stepSendMessages)
+}
+
+func (s *SMRegisterOnLMN) stepRegisterIncomingResult(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	if s.IncomingResult.Type != execution.Done {
+		panic(throw.IllegalState())
+	}
+
+	var (
+		haveFilament  = true
+		isConstructor = s.IncomingResult.Result.Type() == requestresult.SideEffectActivate
+		isDestructor  = s.IncomingResult.Result.Type() == requestresult.SideEffectDeactivate
+		isNone        = s.IncomingResult.Result.Type() == requestresult.SideEffectNone
+	)
+
+	{ // result of execution
+		prevRef := s.LastFilamentRef
+		if prevRef.IsEmpty() {
+			haveFilament = false
+			prevRef = s.LastLifelineRef
+		}
+		if prevRef.IsEmpty() {
+			panic(throw.IllegalState())
+		}
+
+		record := &rms.RInboundResponse{
+			RootRef: rms.NewReference(s.Object),
+			PrevRef: rms.NewReference(prevRef),
+		}
+
+		anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
+
+		s.messages = append(s.messages, &rms.LRegisterRequest{
+			AnticipatedRef: rms.NewReference(anticipatedRef),
+			Flags:          rms.RegistrationFlags_Safe,
+			AnyRecordLazy:  mustRecordToAnyRecordLazy(record),
+		})
+
+		s.LastFilamentRef = anticipatedRef
+	}
+
+	// TODO: RejoinRef to LastFilamentRef
+	{ // new memory (if needed)
+		if s.LastLifelineRef.IsEmpty() {
+			panic(throw.IllegalState())
+		}
+
+		var record rms.BasicRecord
+
+		switch {
+		case !haveFilament && isConstructor:
+			record = &rms.RLineMemoryInit{
+				RootRef: rms.NewReference(s.Object),
+				PrevRef: rms.NewReference(s.LastLifelineRef),
+			}
+		case isDestructor:
+			record = nil
+		case isNone:
+			// TODO: we should post here a link to previous memory
+			record = &rms.RLineMemoryReuse{
+				RootRef: rms.NewReference(s.Object),
+				PrevRef: rms.NewReference(s.LastLifelineRef),
+			}
+		default:
+			record = &rms.RLineMemory{
+				RootRef: rms.NewReference(s.Object),
+				PrevRef: rms.NewReference(s.LastLifelineRef),
+			}
+		}
+
+		if record != nil {
+			anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
+
+			s.messages = append(s.messages, &rms.LRegisterRequest{
+				AnticipatedRef: rms.NewReference(anticipatedRef),
+				Flags:          rms.RegistrationFlags_Safe,
+				AnyRecordLazy:  mustRecordToAnyRecordLazy(record),
+			})
+
+			s.LastLifelineRef = anticipatedRef
 		}
 	}
 
-	req := &rms.LRegisterRequest{
-		Flags:   rms.RegistrationFlags_FastSafe,
-		Records: lazyRecords,
-	}
+	// TODO: RejoinRef to LastFilamentRef
+	{
+		var record rms.BasicRecord
 
-	s.messages = append(s.messages, req)
+		switch {
+		case isConstructor:
+			record = &rms.RLineActivate{
+				RootRef: rms.NewReference(s.Object),
+				PrevRef: rms.NewReference(s.LastLifelineRef),
+			}
+		case isDestructor:
+			record = &rms.RLineDeactivate{
+				RootRef: rms.NewReference(s.Object),
+				PrevRef: rms.NewReference(s.LastLifelineRef),
+			}
+		default:
+			record = nil
+		}
+
+		if record != nil {
+			anticipatedRef := s.getRecordAnticipatedRef(s.Object, record)
+
+			s.messages = append(s.messages, &rms.LRegisterRequest{
+				AnticipatedRef: rms.NewReference(anticipatedRef),
+				Flags:          rms.RegistrationFlags_Safe,
+				AnyRecordLazy:  mustRecordToAnyRecordLazy(record),
+			})
+
+			s.LastLifelineRef = anticipatedRef
+		}
+	}
 
 	return ctx.Jump(s.stepSendMessages)
 }
 
 func (s *SMRegisterOnLMN) stepSendMessages(ctx smachine.ExecutionContext) smachine.StateUpdate {
 	var (
-		object       = s.object
+		object       = s.Object
 		messages     = s.messages
 		currentPulse = s.pulseSlot.CurrentPulseNumber()
 	)
 
-	s.messageSender.PrepareAsync(ctx, func(goCtx context.Context, svc messagesender.Service) smachine.AsyncResultFunc {
+	if len(messages) == 0 {
+		panic(throw.IllegalState())
+	}
+
+	return s.messageSender.PrepareAsync(ctx, func(goCtx context.Context, svc messagesender.Service) smachine.AsyncResultFunc {
 		for _, msg := range messages {
 			err := svc.SendRole(goCtx, msg, affinity.DynamicRoleLightExecutor, object, currentPulse)
 			if err != nil {
@@ -251,12 +407,14 @@ func (s *SMRegisterOnLMN) stepSendMessages(ctx smachine.ExecutionContext) smachi
 		}
 
 		return nil
-	}).WithoutAutoWakeUp().Start()
-
-	return ctx.Jump(s.stepWaitResponses)
+	}).DelayedStart().ThenJump(s.stepWaitResponses)
 }
 
 func (s *SMRegisterOnLMN) stepWaitResponses(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	s.NewLastLifelineRef = s.LastLifelineRef
+	s.NewLastFilamentRef = s.LastFilamentRef
+	s.NewObjectRef = s.Object
+
 	// TODO: we should wait here for given number of FAST (and maybe safe) responses, so we should set
 	//       the right types and kinds of barge-ins in stepRegister***
 	return ctx.Stop()
