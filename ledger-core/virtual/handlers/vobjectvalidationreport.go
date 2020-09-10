268e52a7025a0769a3e6a9162cc6213fa0b718fa
Ekaterina Rogushkova <catherine.zamaraeva@gmail.com>
2020-09-10 14:33:06 +0300

    PLAT-763: send VCachedMemoryRequest in SMVObjectValidationReport

--- ledger-core/virtual/handlers/vobjectvalidationreport.go
+++ ledger-core/virtual/handlers/vobjectvalidationreport.go
@@ -6,8 +6,14 @@
 package handlers
 
 import (
+	"context"
+
+	"github.com/insolar/assured-ledger/ledger-core/appctl/affinity"
+	"github.com/insolar/assured-ledger/ledger-core/conveyor"
 	"github.com/insolar/assured-ledger/ledger-core/conveyor/smachine"
 	"github.com/insolar/assured-ledger/ledger-core/insolar/payload"
+	"github.com/insolar/assured-ledger/ledger-core/network/messagesender"
+	messageSenderAdapter "github.com/insolar/assured-ledger/ledger-core/network/messagesender/adapter"
 	"github.com/insolar/assured-ledger/ledger-core/vanilla/injector"
 )
 
@@ -15,6 +21,10 @@ type SMVObjectValidationReport struct {
 	// input arguments
 	Meta    *payload.Meta
 	Payload *payload.VObjectValidationReport
+
+	// dependencies
+	pulseSlot     *conveyor.PulseSlot
+	messageSender messageSenderAdapter.MessageSender
 }
 
 /* -------- Declaration ------------- */
@@ -25,7 +35,11 @@ type dSMVObjectValidationReport struct {
 	smachine.StateMachineDeclTemplate
 }
 
-func (*dSMVObjectValidationReport) InjectDependencies(_ smachine.StateMachine, _ smachine.SlotLink, _ injector.DependencyInjector) {
+func (*dSMVObjectValidationReport) InjectDependencies(sm smachine.StateMachine, _ smachine.SlotLink, injector injector.DependencyInjector) {
+	s := sm.(*SMVObjectValidationReport)
+
+	injector.MustInject(&s.pulseSlot)
+	injector.MustInject(&s.messageSender)
 }
 
 func (*dSMVObjectValidationReport) GetInitStateFor(sm smachine.StateMachine) smachine.InitFunc {
@@ -40,5 +54,22 @@ func (s *SMVObjectValidationReport) GetStateMachineDeclaration() smachine.StateM
 }
 
 func (s *SMVObjectValidationReport) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
+	return ctx.Jump(s.stepProcess)
+}
+
+func (s *SMVObjectValidationReport) stepProcess(ctx smachine.ExecutionContext) smachine.StateUpdate {
+	msg := payload.VCachedMemoryRequest{
+		Object:  s.Payload.Object,
+		StateID: s.Payload.Validated.GetLocal(),
+	}
+
+	s.messageSender.PrepareAsync(ctx, func(goCtx context.Context, svc messagesender.Service) smachine.AsyncResultFunc {
+		err := svc.SendRole(goCtx, &msg, affinity.DynamicRoleVirtualValidator, s.Payload.Object, s.pulseSlot.CurrentPulseNumber())
+		return func(ctx smachine.AsyncResultContext) {
+			if err != nil {
+				ctx.Log().Error("failed to send message", err)
+			}
+		}
+	}).WithoutAutoWakeUp().Start()
 	return ctx.Stop()
 }
