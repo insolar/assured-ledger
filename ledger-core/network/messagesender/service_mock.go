fc9d370de3717ef9f8099c665d5fe95a3a24eb58
Red Cyrax <cyraxred@users.noreply.github.com>
2020-09-11 21:59:47 +0300

    Fix generate

--- ledger-core/network/messagesender/service_mock.go
+++ ledger-core/network/messagesender/service_mock.go
@@ -9,7 +9,6 @@ import (
 	mm_time "time"
 
 	"github.com/gojuno/minimock/v3"
-
 	"github.com/insolar/assured-ledger/ledger-core/appctl/affinity"
 	"github.com/insolar/assured-ledger/ledger-core/pulse"
 	"github.com/insolar/assured-ledger/ledger-core/reference"
