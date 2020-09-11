11bb8cf8e30ddcd299937fe66273f72d3e5c56ee
Red Cyrax <cyraxred@users.noreply.github.com>
2020-09-11 21:55:06 +0300

    Fix generate

--- ledger-core/testutils/network/host_network_mock.go
+++ ledger-core/testutils/network/host_network_mock.go
@@ -9,7 +9,6 @@ import (
 	mm_time "time"
 
 	"github.com/gojuno/minimock/v3"
-
 	mm_network "github.com/insolar/assured-ledger/ledger-core/network"
 	"github.com/insolar/assured-ledger/ledger-core/network/hostnetwork/packet/types"
 	"github.com/insolar/assured-ledger/ledger-core/reference"
--- ledger-core/testutils/network/routing_table_mock.go
+++ ledger-core/testutils/network/routing_table_mock.go
@@ -8,7 +8,6 @@ import (
 	mm_time "time"
 
 	"github.com/gojuno/minimock/v3"
-
 	"github.com/insolar/assured-ledger/ledger-core/reference"
 	"github.com/insolar/assured-ledger/ledger-core/rms/legacyhost"
 )
