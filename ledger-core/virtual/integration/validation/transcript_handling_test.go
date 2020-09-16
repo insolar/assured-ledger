313e7428e710ccc2219b1d0e6045f0dcd4413ff5
Ekaterina Rogushkova <catherine.zamaraeva@gmail.com>
2020-09-16 17:42:04 +0300

    add test for VObjectTranscriptReport after method

--- ledger-core/virtual/integration/validation/transcript_handling_test.go
+++ ledger-core/virtual/integration/validation/transcript_handling_test.go
@@ -11,11 +11,18 @@ import (
 
 	"github.com/gojuno/minimock/v3"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"github.com/insolar/assured-ledger/ledger-core/insolar/contract"
+	"github.com/insolar/assured-ledger/ledger-core/insolar/contract/isolation"
 	"github.com/insolar/assured-ledger/ledger-core/reference"
 	"github.com/insolar/assured-ledger/ledger-core/rms"
+	"github.com/insolar/assured-ledger/ledger-core/runner/execution"
+	"github.com/insolar/assured-ledger/ledger-core/runner/requestresult"
 	commontestutils "github.com/insolar/assured-ledger/ledger-core/testutils"
+	"github.com/insolar/assured-ledger/ledger-core/testutils/runner/logicless"
 	"github.com/insolar/assured-ledger/ledger-core/virtual/authentication"
+	"github.com/insolar/assured-ledger/ledger-core/virtual/descriptor"
 	"github.com/insolar/assured-ledger/ledger-core/virtual/handlers"
 	"github.com/insolar/assured-ledger/ledger-core/virtual/integration/utils"
 )
@@ -67,6 +74,97 @@ func TestValidation_ObjectTranscriptReport_AfterConstructor(t *testing.T) {
 	commontestutils.WaitSignalsTimed(t, 10*time.Second, server.Journal.WaitAllAsyncCallsDone())
 
 	assert.Equal(t, 1, typedChecker.VObjectTranscriptReport.Count())
+
+	mc.Finish()
+}
+
+func TestValidation_ObjectTranscriptReport_AfterMethod(t *testing.T) {
+	defer commontestutils.LeakTester(t)
+	mc := minimock.NewController(t)
+
+	server, ctx := utils.NewUninitializedServer(nil, t)
+	defer server.Stop()
+
+	authService := authentication.NewServiceMock(t)
+	authService.CheckMessageFromAuthorizedVirtualMock.Return(false, nil)
+	server.ReplaceAuthenticationService(authService)
+
+	runnerMock := logicless.NewServiceMock(ctx, mc, nil)
+	server.ReplaceRunner(runnerMock)
+
+	server.Init(ctx)
+
+	callRequest := utils.GenerateVCallRequestMethod(server)
+	objectRef := callRequest.Callee
+	outgoing := callRequest.CallOutgoing
+	p := server.GetPulse().PulseNumber
+
+	objDescriptor := descriptor.NewObject(objectRef.GetValue(), server.RandomLocalWithPulse(), server.RandomGlobalWithPulse(), []byte("init state"), false)
+	stateRef := reference.NewRecordOf(objDescriptor.HeadRef(), objDescriptor.StateID())
+	var newStateRef reference.Global
+
+	// add typedChecker
+	typedChecker := server.PublisherMock.SetTypedChecker(ctx, mc, server)
+	{
+		typedChecker.VCachedMemoryRequest.Set(func(report *rms.VCachedMemoryRequest) bool {
+			require.Equal(t, stateRef.GetLocal(), report.StateID.GetValue().GetLocal())
+
+			pl := &rms.VCachedMemoryResponse{
+				Object:     report.Object,
+				StateID:    report.StateID,
+				CallStatus: rms.CachedMemoryStateFound,
+				Memory:     rms.NewBytes(objDescriptor.Memory()),
+			}
+			server.SendPayload(ctx, pl)
+			return false
+		})
+	}
+
+	// add runnerMock
+	{
+		newObjDescr := descriptor.NewObject(objectRef.GetValue(), server.RandomLocalWithPulse(), server.RandomGlobalWithPulse(), []byte("new state"), false)
+		newStateRef = reference.NewRecordOf(newObjDescr.HeadRef(), newObjDescr.StateID())
+		requestResult := requestresult.New([]byte("call result"), server.RandomGlobalWithPulse())
+		requestResult.SetAmend(objDescriptor, []byte("new state"))
+		runnerMock.AddExecutionClassify(outgoing.GetValue(), contract.MethodIsolation{Interference: isolation.CallTolerable, State: isolation.CallDirty}, nil)
+		runnerMock.AddExecutionMock(outgoing.GetValue()).AddStart(
+			nil,
+			&execution.Update{
+				Type:   execution.Done,
+				Result: requestResult,
+			},
+		)
+	}
+
+	// send VObjectTranscriptReport
+	{
+		pl := rms.VObjectTranscriptReport{
+			AsOf:   p,
+			Object: objectRef,
+			ObjectTranscript: rms.VObjectTranscriptReport_Transcript{
+				Entries: []rms.Any{{}, {}},
+			},
+		}
+		pl.ObjectTranscript.Entries[0].Set(
+			&rms.VObjectTranscriptReport_TranscriptEntryIncomingRequest{
+				ObjectMemory: rms.NewReference(stateRef),
+				Request:      *callRequest,
+			},
+		)
+		pl.ObjectTranscript.Entries[1].Set(
+			&rms.VObjectTranscriptReport_TranscriptEntryIncomingResult{
+				IncomingResult: rms.NewReference(reference.Global{}),
+				ObjectState:    rms.NewReference(newStateRef),
+			},
+		)
+
+		done := server.Journal.WaitStopOf(&handlers.SMVObjectTranscriptReport{}, 1)
+		server.SendPayload(ctx, &pl)
+		commontestutils.WaitSignalsTimed(t, 10*time.Second, done)
+		commontestutils.WaitSignalsTimed(t, 10*time.Second, server.Journal.WaitAllAsyncCallsDone())
+
+		assert.Equal(t, 1, typedChecker.VCachedMemoryRequest.Count())
+	}
 
 	mc.Finish()
 }
