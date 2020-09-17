89616a1ecba21d40bd9e5befda13a606a39a8cc2
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2020-09-17 14:16:55 +0300

    PLAT-825: Implement wrappers for factories and transports (test purpose) (#777)

---
+++ ledger-core/network/nds/uniproto/l2/uniserver/test_helpers.go
@@ -0,0 +1,51 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package uniserver
+
+import (
+	"crypto/tls"
+	"github.com/insolar/assured-ledger/ledger-core/network/nds/uniproto/l1"
+	"github.com/insolar/assured-ledger/ledger-core/network/nwapi"
+)
+
+type FuncMapSessionlessProvider func(l1.SessionlessTransportProvider) l1.SessionlessTransportProvider
+type FuncMapSessionfullProvider func(l1.SessionfulTransportProvider) l1.SessionfulTransportProvider
+
+func MapTransportProvider(
+	provider AllTransportProvider,
+	mappingLessFunc FuncMapSessionlessProvider,
+	mappingFullFunc FuncMapSessionfullProvider,
+) AllTransportProvider {
+	return &MappedTransportProvider{
+		AllTransportProvider: provider,
+		mappingLessFunc:      mappingLessFunc,
+		mappingFullFunc:      mappingFullFunc,
+	}
+}
+
+type MappedTransportProvider struct {
+	AllTransportProvider
+	mappingLessFunc FuncMapSessionlessProvider
+	mappingFullFunc FuncMapSessionfullProvider
+}
+
+func (p *MappedTransportProvider) CreateSessionlessProvider(binding nwapi.Address, preference nwapi.Preference, maxUDPSize uint16) l1.SessionlessTransportProvider {
+	sessionlessProvider := p.AllTransportProvider.CreateSessionlessProvider(binding, preference, maxUDPSize)
+
+	if p.mappingLessFunc != nil {
+		return p.mappingLessFunc(sessionlessProvider)
+	}
+	return sessionlessProvider
+}
+
+func (p *MappedTransportProvider) CreateSessionfulProvider(binding nwapi.Address, preference nwapi.Preference, tlsCfg *tls.Config) l1.SessionfulTransportProvider {
+	sessionfullProvider := p.AllTransportProvider.CreateSessionfulProvider(binding, preference, tlsCfg)
+
+	if p.mappingFullFunc != nil {
+		return p.mappingFullFunc(sessionfullProvider)
+	}
+	return sessionfullProvider
+}
