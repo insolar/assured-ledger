5c03e0127800e14372f7d31c84145201a6064f16
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2020-09-23 01:39:11 +0300

    NOISSUE: WIP

--- ledger-core/network/nds/msgdelivery/controller_bench_test.go
+++ ledger-core/network/nds/msgdelivery/controller_bench_test.go
@@ -13,12 +13,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/insolar/assured-ledger/ledger-core/network/nds/uniproto"
 	"github.com/insolar/assured-ledger/ledger-core/network/nds/uniproto/l2/uniserver"
 	"github.com/insolar/assured-ledger/ledger-core/network/nwapi"
-	"github.com/insolar/assured-ledger/ledger-core/pulse"
-	"github.com/insolar/assured-ledger/ledger-core/vanilla/cryptkit"
-	"github.com/insolar/assured-ledger/ledger-core/vanilla/longbits"
 )
 
 type BenchType func(v benchSender, payload []byte)
@@ -33,12 +29,33 @@ func BenchmarkThroughput(b *testing.B) {
 	}()
 
 	results := make(chan []byte, 16)
-	sender, stopFn := createPipe(b, "127.0.0.1:0", "127.0.0.1:0", 0, func(bb []byte) {
-		results <- bb
-	})
-	defer stopFn()
 
-	sender.results = results
+	h := NewUnitProtoServersHolder(TestLogAdapter{t: b})
+	defer h.stop()
+
+	prf := &UnitProtoServerProfile{
+		config: uniserver.ServerConfig{
+			BindingAddress: "127.0.0.1:0",
+			UDPMaxSize:     0,
+			UDPParallelism: 4,
+			PeerLimit:      -1,
+		},
+		desFactory: &TestDeserializationByteFactory{},
+	}
+
+	srv1, err := h.createServiceWithProfile(prf, func(a ReturnAddress, done nwapi.PayloadCompleteness, val interface{}) error {
+		bb := val.(*TestBytes).S
+		results <- bb
+		return nil
+	})
+	require.NoError(b, err)
+
+	srv2, err := h.createServiceWithProfile(prf, func(a ReturnAddress, done nwapi.PayloadCompleteness, val interface{}) error {
+		bb := val.(*TestBytes).S
+		results <- bb
+		return nil
+	})
+	require.NoError(b, err)
 
 	testCases := []BenchType{
 		shipToBody,
@@ -46,65 +63,70 @@ func BenchmarkThroughput(b *testing.B) {
 	}
 
 	for _, runFunc := range testCases {
-		bench := sender
+		b.Run("remote", func(b *testing.B) {
+			bench := benchSender{
+				toAddr:  srv1.directAddress(),
+				results: results,
+				ctl:     srv2.service,
+			}
 
-		b.Run("0.1k", func(b *testing.B) {
-			bench.throughput(b, 100, runFunc)
+			b.Run("0.1k", func(b *testing.B) {
+				bench.throughput(b, 100, runFunc)
+			})
+
+			b.Run("1k", func(b *testing.B) {
+				bench.throughput(b, 1<<10, runFunc)
+			})
+
+			b.Run("4k", func(b *testing.B) {
+				bench.throughput(b, 1<<12, runFunc)
+			})
+
+			b.Run("16k", func(b *testing.B) {
+				bench.throughput(b, 1<<14, runFunc)
+			})
+
+			b.Run("128k", func(b *testing.B) {
+				bench.throughput(b, 1<<17, runFunc)
+			})
+
+			b.Run("1M", func(b *testing.B) {
+				bench.throughput(b, 1<<20, runFunc)
+			})
 		})
 
-		b.Run("1k", func(b *testing.B) {
-			bench.throughput(b, 1<<10, runFunc)
-		})
+		b.Run("loopback", func(b *testing.B) {
+			bench := benchSender{
+				toAddr:  srv2.directAddress(), // loopback
+				results: results,
+				ctl:     srv2.service,
+			}
 
-		b.Run("4k", func(b *testing.B) {
-			bench.throughput(b, 1<<12, runFunc)
-		})
+			b.Run("0.1k", func(b *testing.B) {
+				bench.throughput(b, 100, runFunc)
+			})
 
-		b.Run("16k", func(b *testing.B) {
-			bench.throughput(b, 1<<14, runFunc)
-		})
+			b.Run("1k", func(b *testing.B) {
+				bench.throughput(b, 1<<10, runFunc)
+			})
 
-		b.Run("128k", func(b *testing.B) {
-			bench.throughput(b, 1<<17, runFunc)
-		})
+			b.Run("4k", func(b *testing.B) {
+				bench.throughput(b, 1<<12, runFunc)
+			})
 
-		b.Run("1M", func(b *testing.B) {
-			bench.throughput(b, 1<<20, runFunc)
-		})
+			b.Run("16k", func(b *testing.B) {
+				bench.throughput(b, 1<<14, runFunc)
+			})
 
-		b.Run("8M", func(b *testing.B) {
-			bench.throughput(b, 1<<23, runFunc)
-		})
+			b.Run("128k", func(b *testing.B) {
+				bench.throughput(b, 1<<17, runFunc)
+			})
 
-		b.Run("32M", func(b *testing.B) {
-			bench.throughput(b, 1<<25, runFunc)
-		})
-
-		b.Run("64M", func(b *testing.B) {
-			bench.throughput(b, 1<<26, runFunc)
-		})
-
-		b.Run("128M", func(b *testing.B) {
-			bench.throughput(b, 1<<27, runFunc)
+			b.Run("1M", func(b *testing.B) {
+				bench.throughput(b, 1<<20, runFunc)
+			})
 		})
 	}
-
-	// b.Run("loopback", func(b *testing.B) {
-	// 	bench := sender
-	// 	bench.toAddr = NewDirectAddress(2) // loopback
-	//
-	// 	b.Run("0.1k", func(b *testing.B) {
-	// 		bench.throughput(b, 100)
-	// 	})
-	//
-	// 	b.Run("4k", func(b *testing.B) {
-	// 		bench.throughput(b, 1<<12)
-	// 	})
-	//
-	// 	b.Run("16k", func(b *testing.B) {
-	// 		bench.throughput(b, 1<<14)
-	// 	})
-	// })
 }
 
 // WARNING! Benchmark is unstable due to packet drops on overflow.
@@ -117,14 +139,31 @@ func BenchmarkLatency(b *testing.B) {
 	}()
 
 	results := make(chan []byte, 1)
-	sender, stopFn := createPipe(b, "127.0.0.1:0", "127.0.0.1:0", 0, func(bb []byte) {
-		// nanos := time.Now().UnixNano()
-		// nanos -= int64(binary.LittleEndian.Uint64(bb))
-		results <- nil
-	})
-	defer stopFn()
 
-	sender.results = results
+	h := NewUnitProtoServersHolder(TestLogAdapter{t: b})
+	defer h.stop()
+
+	prf := &UnitProtoServerProfile{
+		config: uniserver.ServerConfig{
+			BindingAddress: "127.0.0.1:0",
+			UDPMaxSize:     0,
+			UDPParallelism: 4,
+			PeerLimit:      -1,
+		},
+		desFactory: &TestDeserializationByteFactory{},
+	}
+
+	srv1, err := h.createServiceWithProfile(prf, func(a ReturnAddress, done nwapi.PayloadCompleteness, val interface{}) error {
+		results <- nil
+		return nil
+	})
+	require.NoError(b, err)
+
+	srv2, err := h.createServiceWithProfile(prf, func(a ReturnAddress, done nwapi.PayloadCompleteness, val interface{}) error {
+		results <- nil
+		return nil
+	})
+	require.NoError(b, err)
 
 	testCases := []BenchType{
 		shipToBody,
@@ -132,145 +171,86 @@ func BenchmarkLatency(b *testing.B) {
 	}
 
 	for _, runFunc := range testCases {
-		bench := sender
-
-		b.Run("0.1k", func(b *testing.B) {
-			bench.latency(b, 100, runFunc)
-		})
-
-		b.Run("4k", func(b *testing.B) {
-			bench.latency(b, 1<<12, runFunc)
-		})
-
-		b.Run("128k", func(b *testing.B) {
-			bench.latency(b, 1<<17, runFunc)
-		})
-
-		b.Run("1M", func(b *testing.B) {
-			bench.latency(b, 1<<20, runFunc)
-		})
-
-		b.Run("8M", func(b *testing.B) {
-			bench.latency(b, 1<<23, runFunc)
-		})
-
-		b.Run("32M", func(b *testing.B) {
-			bench.latency(b, 1<<25, runFunc)
-		})
-
-		b.Run("64M", func(b *testing.B) {
-			bench.latency(b, 1<<26, runFunc)
-		})
-
-		b.Run("128M", func(b *testing.B) {
-			bench.latency(b, 1<<27, runFunc)
-		})
-	}
-}
-
-func createPipe(t testing.TB, server1, server2 string, udpMaxSize int, resultFn func([]byte)) (benchSender, func()) {
-
-	var idWithPortFn func(nwapi.Address) bool
-	if server1 == server2 {
-		addr := nwapi.NewHostPort(server1, true)
-		if !addr.IsLoopback() {
-			addr = addr.HostIdentity()
-			idWithPortFn = func(address nwapi.Address) bool {
-				return address.HostIdentity() == addr
+		b.Run("remote", func(b *testing.B) {
+			bench := benchSender{
+				toAddr:  srv1.directAddress(),
+				results: results,
+				ctl:     srv2.service,
 			}
-		}
+
+			b.Run("0.1k", func(b *testing.B) {
+				bench.latency(b, 100, runFunc)
+			})
+
+			b.Run("4k", func(b *testing.B) {
+				bench.latency(b, 1<<12, runFunc)
+			})
+
+			b.Run("128k", func(b *testing.B) {
+				bench.latency(b, 1<<17, runFunc)
+			})
+
+			b.Run("1M", func(b *testing.B) {
+				bench.latency(b, 1<<20, runFunc)
+			})
+
+			b.Run("8M", func(b *testing.B) {
+				bench.latency(b, 1<<23, runFunc)
+			})
+
+			b.Run("32M", func(b *testing.B) {
+				bench.latency(b, 1<<25, runFunc)
+			})
+
+			b.Run("64M", func(b *testing.B) {
+				bench.latency(b, 1<<26, runFunc)
+			})
+
+			b.Run("128M", func(b *testing.B) {
+				bench.latency(b, 1<<27, runFunc)
+			})
+		})
+
+		b.Run("loopback", func(b *testing.B) {
+			bench := benchSender{
+				toAddr:  srv2.directAddress(),
+				results: results,
+				ctl:     srv2.service,
+			}
+
+			b.Run("0.1k", func(b *testing.B) {
+				bench.latency(b, 100, runFunc)
+			})
+
+			b.Run("4k", func(b *testing.B) {
+				bench.latency(b, 1<<12, runFunc)
+			})
+
+			b.Run("128k", func(b *testing.B) {
+				bench.latency(b, 1<<17, runFunc)
+			})
+
+			b.Run("1M", func(b *testing.B) {
+				bench.latency(b, 1<<20, runFunc)
+			})
+
+			b.Run("8M", func(b *testing.B) {
+				bench.latency(b, 1<<23, runFunc)
+			})
+
+			b.Run("32M", func(b *testing.B) {
+				bench.latency(b, 1<<25, runFunc)
+			})
+
+			b.Run("64M", func(b *testing.B) {
+				bench.latency(b, 1<<26, runFunc)
+			})
+
+			b.Run("128M", func(b *testing.B) {
+				bench.latency(b, 1<<27, runFunc)
+			})
+		})
 	}
-
-	vf := TestVerifierFactory{}
-	skBytes := [testDigestSize]byte{}
-	sk1 := cryptkit.NewSigningKey(longbits.CopyBytes(skBytes[:]), testSigningMethod, cryptkit.PublicAsymmetricKey)
-	skBytes[0] = 1
-	sk2 := cryptkit.NewSigningKey(longbits.CopyBytes(skBytes[:]), testSigningMethod, cryptkit.PublicAsymmetricKey)
-
-	controller1 := NewController(Protocol, TestDeserializationByteFactory{},
-		func(a ReturnAddress, _ nwapi.PayloadCompleteness, v interface{}) error {
-			resultFn(v.(*TestBytes).S)
-			return nil
-		}, nil, TestLogAdapter{t})
-
-	var dispatcher1 uniserver.Dispatcher
-	controller1.RegisterWith(dispatcher1.RegisterProtocol)
-
-	ups1 := uniserver.NewUnifiedServer(&dispatcher1, TestLogAdapter{t})
-	ups1.SetConfig(uniserver.ServerConfig{
-		BindingAddress: server1,
-		UDPMaxSize:     udpMaxSize,
-		UDPParallelism: 4,
-		PeerLimit:      -1,
-	})
-	ups1.SetIdentityClassifier(idWithPortFn)
-
-	ups1.SetPeerFactory(func(peer *uniserver.Peer) (remapTo nwapi.Address, err error) {
-		peer.SetSignatureKey(sk2)
-		peer.SetNodeID(2)
-		return nwapi.NewHostID(2), nil
-	})
-	ups1.SetSignatureFactory(vf)
-
-	ups1.StartListen()
-	dispatcher1.SetMode(uniproto.AllowAll)
-
-	pm1 := ups1.PeerManager()
-	_, err := pm1.AddHostID(pm1.Local().GetPrimary(), 1)
-	require.NoError(t, err)
-
-	pr := pulse.NewOnePulseRange(pulse.NewFirstPulsarData(5, longbits.Bits256{}))
-	dispatcher1.NextPulse(pr)
-
-	/********************************/
-
-	controller2 := NewController(Protocol, TestDeserializationByteFactory{},
-		func(a ReturnAddress, _ nwapi.PayloadCompleteness, v interface{}) error {
-			resultFn(v.(*TestBytes).S)
-			return nil
-		}, nil, TestLogAdapter{t})
-
-	var dispatcher2 uniserver.Dispatcher
-	dispatcher2.SetMode(uniproto.NewConnectionMode(0, Protocol))
-	controller2.RegisterWith(dispatcher2.RegisterProtocol)
-	dispatcher2.Seal()
-
-	ups2 := uniserver.NewUnifiedServer(&dispatcher2, TestLogAdapter{t})
-	ups2.SetConfig(uniserver.ServerConfig{
-		BindingAddress: server2,
-		UDPMaxSize:     udpMaxSize,
-		UDPParallelism: 4,
-		PeerLimit:      -1,
-	})
-	ups2.SetIdentityClassifier(idWithPortFn)
-
-	ups2.SetPeerFactory(func(peer *uniserver.Peer) (remapTo nwapi.Address, err error) {
-		peer.SetSignatureKey(sk1)
-		peer.SetNodeID(1)
-		return nwapi.NewHostID(1), nil
-	})
-	ups2.SetSignatureFactory(vf)
-
-	ups2.StartListen()
-	dispatcher2.NextPulse(pr)
-
-	pm2 := ups2.PeerManager()
-	_, err = pm2.AddHostID(pm2.Local().GetPrimary(), 2)
-	require.NoError(t, err)
-
-	conn21, err := pm2.Manager().ConnectPeer(pm1.Local().GetPrimary())
-	require.NoError(t, err)
-	require.NoError(t, conn21.Transport().EnsureConnect())
-
-	ctl2 := controller2.NewFacade()
-
-	return benchSender{
-			toAddr: NewDirectAddress(1),
-			ctl:    ctl2,
-		}, func() {
-			dispatcher1.Stop()
-			dispatcher2.Stop()
-		}
 }
 
 type benchSender struct {
