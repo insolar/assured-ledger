31687089ad77bdb0480f8a23aa6d91456f83d4ef
Ekaterina Rogushkova <catherine.zamaraeva@gmail.com>
2020-09-30 14:05:47 +0300

    add func for validator

--- ledger-core/appctl/affinity/affinity.go
+++ ledger-core/appctl/affinity/affinity.go
@@ -50,8 +50,8 @@ func (jc *Coordinator) QueryRole(role DynamicRole, objID reference.Holder, pn pu
 		}
 		return []reference.Global{n}, nil
 	case DynamicRoleVirtualValidator:
-		// fixme: func for validator
-		n, err := jc.VirtualExecutorForObject(objID, pn)
+		// fixme: validator == executor now , need to change
+		n, err := jc.VirtualValidatorForObject(objID, pn)
 		if err != nil {
 			return nil, throw.WithDetails(err, struct {
 				Ref reference.Holder
@@ -66,6 +66,54 @@ func (jc *Coordinator) QueryRole(role DynamicRole, objID reference.Holder, pn pu
 
 // VirtualExecutorForObject returns list of VEs for a provided pulse and objID
 func (jc *Coordinator) VirtualExecutorForObject(objID reference.Holder, pn pulse.Number) (reference.Global, error) {
+	pc, err := jc.PulseAccessor.TimeBeat(pn)
+	switch {
+	case err != nil:
+		return reference.Global{}, err
+	case pc.Online == nil:
+		return reference.Global{}, throw.IllegalState()
+	}
+
+	role := pc.Online.GetRolePopulation(member.PrimaryRoleVirtual)
+	if role == nil {
+		return reference.Global{}, throw.E("role without nodes", struct {
+			PrimaryRole member.PrimaryRole
+			Population  census.OnlinePopulation
+		}{
+			member.PrimaryRoleVirtual,
+			pc.Online,
+		})
+	}
+
+	base := objID.GetBase()
+	h := sha256.New()
+	_, err = h.Write(base.AsBytes())
+	if err != nil {
+		return reference.Global{}, err
+	}
+	_, err = h.Write(pc.PulseEntropy[:])
+	if err != nil {
+		return reference.Global{}, err
+	}
+	metric := longbits.CutOutUint64(h.Sum(nil))
+
+	assigned, _ := role.GetAssignmentByCount(metric, 0)
+	if assigned == nil {
+		return reference.Global{}, throw.E("unable to assign node of role", struct {
+			PrimaryRole member.PrimaryRole
+			Population  census.OnlinePopulation
+		}{
+			member.PrimaryRoleVirtual,
+			pc.Online,
+		})
+	}
+	ref := assigned.GetStatic().GetExtension().GetReference()
+
+	return ref, nil
+}
+
+// VirtualValidatorForObject returns list of VEs for a provided pulse and objID
+func (jc *Coordinator) VirtualValidatorForObject(objID reference.Holder, pn pulse.Number) (reference.Global, error) {
 	pc, err := jc.PulseAccessor.TimeBeat(pn)
 	switch {
 	case err != nil:
