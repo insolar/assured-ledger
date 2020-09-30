745ca0cced6f8554fc1590af760c61c9115e067f
Dmitry Rozhdestvenskiy <yellowmegaman@gmail.com>
2020-09-30 21:41:24 +0300

    NOISSUE: springcleaning (#795)

--- ledger-core/scripts/deploy/kube/80node/configuration/wait-for-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-while true; do
-  if [ "$(kubectl -n placeholder get po bootstrap -o jsonpath="{.status.phase}")" = "Succeeded" ]; then
-    break
-  fi
-  sleep 1s
-done
--- ledger-core/scripts/deploy/kube/apply_prometheus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-kubectl apply -k scripts/deploy/kube/prometheus
-kubectl -n prometheus rollout status sts/prometheus --timeout=80s
