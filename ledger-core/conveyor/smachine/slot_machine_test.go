640c70c358250ce43f1000eee9bdb973f42738ae
Ruslan Zakirov <ruslan.zakirov@gmail.com>
2020-10-01 17:21:01 +0300

    test: migration should be called when event comes after replace before init

--- ledger-core/conveyor/smachine/slot_machine_test.go
+++ ledger-core/conveyor/smachine/slot_machine_test.go
@@ -172,3 +172,105 @@ func TestSlotMachine_RestoreStep(t *testing.T) {
 	iterFn()
 	require.False(t, s.wasContinued)
 }
+
+
+type smReplaceAndMigrate1 struct {
+	smachine.StateMachineDeclTemplate
+
+	migrated bool
+	executed bool
+}
+
+func (s *smReplaceAndMigrate1) GetInitStateFor(_ smachine.StateMachine) smachine.InitFunc {
+	return s.Init
+}
+
+func (s *smReplaceAndMigrate1) GetStateMachineDeclaration() smachine.StateMachineDeclaration {
+	return s
+}
+
+func (s *smReplaceAndMigrate1) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
+	return ctx.Jump(s.stepExecute)
+}
+
+func (s *smReplaceAndMigrate1) stepExecute(ctx smachine.ExecutionContext)  smachine.StateUpdate {
+	return ctx.Replace(func(ctx smachine.ConstructionContext) smachine.StateMachine {
+		return &smReplaceAndMigrate2{migrated: &s.migrated, executed: &s.executed}
+	})
+}
+
+type smReplaceAndMigrate2 struct {
+	smachine.StateMachineDeclTemplate
+
+	migrated *bool
+	executed *bool
+}
+
+func (s *smReplaceAndMigrate2) GetInitStateFor(_ smachine.StateMachine) smachine.InitFunc {
+	return s.Init
+}
+
+func (s *smReplaceAndMigrate2) GetStateMachineDeclaration() smachine.StateMachineDeclaration {
+	return s
+}
+
+func (s *smReplaceAndMigrate2) Init(ctx smachine.InitializationContext) smachine.StateUpdate {
+	return ctx.JumpExt(smachine.SlotStep{
+		Transition: 		s.stepExecute,
+		Migration: func(ctx smachine.MigrationContext) smachine.StateUpdate {
+			*s.migrated = true
+			return ctx.Stop()
+		},
+	})
+}
+
+func (s *smReplaceAndMigrate2) stepExecute(ctx smachine.ExecutionContext)  smachine.StateUpdate {
+	*s.executed = true
+	return ctx.Stop()
+}
+
+func TestSlotMachine_ReplaceAndMigrate(t *testing.T) {
+	ctx := instestlogger.TestContext(t)
+
+	scanCountLimit := 1000
+
+	signal := synckit.NewVersionedSignal()
+	m := smachine.NewSlotMachine(smachine.SlotMachineConfig{
+		SlotPageSize:    1000,
+		PollingPeriod:   10 * time.Millisecond,
+		PollingTruncate: 1 * time.Microsecond,
+		ScanCountLimit:  scanCountLimit,
+	}, signal.NextBroadcast, signal.NextBroadcast, nil)
+
+	workerFactory := sworker.NewAttachableSimpleSlotWorker()
+	neverSignal := synckit.NewNeverSignal()
+
+	s := smReplaceAndMigrate1{}
+	m.AddNew(ctx, &s, smachine.CreateDefaultValues{})
+
+	if !m.ScheduleCall(func(callContext smachine.MachineCallContext) {
+		callContext.Migrate(nil)
+	}, true) {
+		panic(throw.IllegalState())
+	}
+
+	// make 1 iteration
+	for {
+		var (
+			repeatNow bool
+		)
+
+		workerFactory.AttachTo(m, neverSignal, uint32(scanCountLimit), func(worker smachine.AttachedSlotWorker) {
+			repeatNow, _ = m.ScanOnce(0, worker)
+		})
+
+		if repeatNow {
+			continue
+		}
+
+		break
+	}
+
+	assert.True(t, s.migrated)
+	assert.False(t, s.executed)
+}
