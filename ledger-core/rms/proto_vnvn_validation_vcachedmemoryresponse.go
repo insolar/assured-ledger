2cfc307fb0a4fa0c5df9574a223f9276b42dbaa2
anton.alekseev <anton.a.alekseev@gmail.com>
2020-10-07 14:02:49 +0300

    PLAT-846 Validate methods for VCachedMemoryRequest/Response

---
+++ ledger-core/rms/proto_vnvn_validation_vcachedmemoryresponse.go
@@ -0,0 +1,43 @@
+// Copyright 2020 Insolar Network Ltd.
+// All rights reserved.
+// This material is licensed under the Insolar License version 1.0,
+// available at https://github.com/insolar/assured-ledger/blob/master/LICENSE.md.
+
+package rms
+
+import (
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/throw"
+)
+
+func (m *VCachedMemoryResponse) Validate(currentPulse PulseNumber) error {
+	object := m.Object.GetValue()
+	_, err := validSelfScopedGlobalWithPulseBeforeOrEq(object, currentPulse, "Object")
+	if err != nil {
+		return err
+	}
+
+	switch m.CallStatus {
+	case CachedMemoryStateUnknown:
+		if m.Object.IsEmpty() {
+			return throw.New("Object should not be empty")
+		}
+		if m.StateID.IsEmpty() {
+			return throw.New("StateID should not be empty")
+		}
+	case CachedMemoryStateFound:
+		if m.Object.IsEmpty() {
+			return throw.New("Object should not be empty")
+		}
+		if m.StateID.IsEmpty() {
+			return throw.New("StateID should not be empty")
+		}
+		if m.Memory.IsEmpty() {
+			return throw.New("Memory should not be empty")
+		}
+
+	default:
+		return throw.New("unexpected CallStatus")
+	}
+
+	return nil
+}
