e68ee94429a8b26ed1caa8721133149cb6262dcd
Andrey Bronin <andrey.bronin@insolar.io>
2020-10-15 20:29:53 +0300

    Descriptor.IsSupported() possible bug comment

--- ledger-core/network/nds/uniproto/api_descriptor.go
+++ ledger-core/network/nds/uniproto/api_descriptor.go
@@ -5,7 +5,9 @@
 
 package uniproto
 
-import "github.com/insolar/assured-ledger/ledger-core/vanilla/cryptkit"
+import (
+	"github.com/insolar/assured-ledger/ledger-core/vanilla/cryptkit"
+)
 
 // Descriptor provides information about a protocol.
 // Each protocol has to define at least packet type #0.
@@ -15,19 +17,20 @@ type Descriptor struct {
 	// Certificate / signature? then needs unified_packet
 
 	// Supporter is optional
-	Supporter        Supporter
+	Supporter Supporter
 	// SupportedPackets is a list of packet types supported by the protocol.
 	// There are some restrictions on protocol type / packet type combinations. See ProtocolType.
 	SupportedPackets PacketDescriptors
 }
 
 func (d Descriptor) IsSupported() bool {
+	// todo: possible  bug
 	return d.SupportedPackets[0].IsSupported()
 }
 
 // PacketDescriptor defines flags and packet size limit for a given packet type of a protocol.
 type PacketDescriptor struct {
-	Flags      Flags
+	Flags Flags
 	// LengthBits is a number of bits used to represent maximum size of a packet.
 	// When LengthBits is [0..4], less than MinLengthBits, then the packet is considered as disabled.
 	// When LengthBits is over ExcessiveLengthBits then this packet can't be supported yet.
